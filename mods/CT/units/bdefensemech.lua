60325e6c041d153c2f006184ace0ee5c671041eb
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2010-06-26 22:10:26 +0000

    added CEGs

---
+++ mods/CT/units/bdefensemech.lua
@@ -0,0 +1,182 @@
+	unitDef = {
+	  unitname            = [[bdefensemech]],
+	  name                = [[Taira Defense Mech]],
+	  description         = [[Base Defense Mech - Weak, first generation SM packed inside Flagships for early defense deployment. - Build Limit: 10]],
+	  acceleration        = 0.04,
+	  bmcode              = [[1]],
+	  brakeRate           = 0.02,
+	  buildCostEnergy     = 50,
+	  buildCostMetal      = 40,
+	  builder             = false,
+	  buildPic            = [[bdefensemech.png]],
+	  buildTime           = 7,
+	  canAttack           = true,
+	  canGuard            = true,
+	  canMove             = true,
+	  canPatrol           = true,
+	  canstop             = [[1]],
+	  category            = [[LAND]],
+	  corpse              = [[DEAD]],
+	  reclaimable         = false,
+	
+	  defaultmissiontype  = [[Standby]],
+	  explodeAs           = [[SMALL_UNITEX]],
+	  footprintX          = 3,
+	  footprintZ          = 3,
+	  idleAutoHeal        = 0,
+	  leaveTracks         = true,
+	  maneuverleashlength = [[640]],
+	  mass                = 90,
+	  maxDamage           = 250,
+	  maxSlope            = 18,
+	  maxVelocity         = 2.5,
+	  maxWaterDepth       = 22,
+	  minCloakDistance    = 75,
+	  movementClass       = [[TANK3]],
+	  moveState           = 0,
+	  noAutoFire          = false,
+	  noChaseCategory     = [[TERRAFORM FIXEDWING SATELLITE SUB]],
+	  objectName          = [[bdefensemech.s3o]],
+	  seismicSignature    = 4,
+	  selfDestructAs      = [[SMALL_UNITEX]],
+
+	  sfxtypes            = {
+	
+	    explosiongenerators = {
+		"custom:orc_machinegun_flash",
+		"custom:orc_machinegun_muzzle",
+	    },
+	
+	  },
+
+
+
+	  sounds			= {
+	      select = {
+		"golgotha/vehicle_done_44khz",
+		},
+	      ok = {
+		"golgotha/vehicle_done_44khz",
+		},
+	  },
+
+	  side                = [[NKG]],
+	  sightDistance       = 300,
+	  smoothAnim          = true,
+	  steeringmode        = [[1]],
+	  TEDClass            = [[TANK]],
+	  trackOffset         = 3,
+	  trackStrength       = 6,
+	  trackStretch        = 1,
+	  trackType           = [[StdTank]],
+	  trackWidth          = 24,
+	  turninplace         = 0,
+	  turnRate            = 550,
+	  upright             = false,
+	  workerTime          = 0,
+	  unitRestricted      = 10,
+          script              = [[bdefensemech.lua]],
+	
+	  weapons             = {
+	
+	    {
+	      def                = [[Machine_Gun]],
+	      badTargetCategory  = [[FIXEDWING GUNSHIP]],
+	      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+	    },
+
+	    {
+	      def                = [[Machine_Gun]],
+	      badTargetCategory  = [[FIXEDWING GUNSHIP]],
+	      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+	    },
+	
+	  },
+	
+	
+	  weaponDefs          = {
+	
+	    Machine_Gun = {
+	      name                    = [[Rapid Fire Cannon]],
+	      areaOfEffect            = 8,
+	      craterMult              = 0.25,
+	
+	      damage                  = {
+		Cruiser = 30,
+		Building = 5,
+		Mech = 30,
+		Tank = 30,
+		Aircraft = 30,
+		Ship = 30,
+		Sub = 30,
+	      },
+	
+	      explosionGenerator      = [[custom:EMG_HIT]],
+	      interceptedByShieldType = 1,
+	      lineOfSight             = true,
+	      range                   = 300,
+	      reloadtime              = 0.2,
+	      rgbColor                = [[1 0.95 0.4]],
+	      separation              = 2,
+              size                    = 1.25,
+	      soundStart              = [[ct/machine_gun]],
+	      soundStartVolume        = 0.5,
+	      soundTrigger            = true,
+	      stages                  = 50,
+	      sprayAngle              = 1180,
+	      tolerance               = 8000,
+	      turret                  = true,
+	      weaponType              = [[Cannon]],
+	      weaponVelocity          = 750,
+	    },
+	
+	  },
+	
+	
+	  featureDefs         = {
+	
+	    DEAD  = {
+	      description      = [[Wreckage - Defense Mech]],
+	      blocking         = false,
+	      category         = [[corpses]],
+	      damage           = 160,
+	      energy           = 0,
+	      featureDead      = [[DEAD2]],
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 2,
+	      footprintZ       = 2,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 15,
+	      object           = [[bgenericwreckage.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	
+	    DEAD2 = {
+	      description      = [[Debris - Defense Mech]],
+	      blocking         = false,
+	      category         = [[heaps]],
+	      damage           = 160,
+	      energy           = 0,
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 3,
+	      footprintZ       = 3,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 15,
+	      object           = [[b3x3heap.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	  },
+	
+	}
+	
+	return lowerkeys({ bdefensemech = unitDef })
