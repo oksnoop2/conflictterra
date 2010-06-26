fa2ca0a7d4acc65af6de010ca7110bd82da7e9c7
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2010-06-26 22:10:13 +0000

    added CEGs

---
+++ mods/CT/units/blras.lua
@@ -0,0 +1,161 @@
+	unitDef = {
+	  unitname            = [[blras]],
+	  name                = [[Minamoto Class Battleship]],
+	  description         = [[Long Range Artillery Ship - Very long range ship armed with a very powerful cannon.  Does more damage to buildings. - Build Limit: 2]],
+	  acceleration        = 0.03,
+	  bmcode              = [[1]],
+	  brakeRate           = 0.03,
+	  buildCostEnergy     = 4600,
+	  buildCostMetal      = 3680,
+	  builder             = false,
+	  buildPic            = [[blras.png]],
+	  buildTime           = 45,
+	  canAttack           = true,
+	  canGuard            = true,
+	  canMove             = true,
+	  canPatrol           = true,
+	  canstop             = [[1]],
+	  category            = [[SHIP]],
+	  corpse              = [[DEAD]],
+          reclaimable         = false,
+	
+	  defaultmissiontype  = [[Standby]],
+	  explodeAs           = [[BIG_UNITEX]],
+          floater             = true,
+	  footprintX          = 8,
+	  footprintZ          = 8,
+          collisionVolumeOffsets = [[0 0 0]],
+          collisionVolumeScales  = [[96 96 296]],
+          collisionVolumeTest    = 1,
+          collisionVolumeType    = [[box]],
+	  idleAutoHeal        = 0,
+	  maneuverleashlength = [[640]],
+	  mass                = 2300,
+	  maxDamage           = 5500,
+	  maxVelocity         = 1,
+	  minCloakDistance    = 75,
+	  minWaterDepth       = 15,
+	  movementClass       = [[BOAT6]],
+	  moveState           = 0,
+	  noAutoFire          = false,
+	  noChaseCategory     = [[FIXEDWING SATELLITE GUNSHIP SUB]],
+	  objectName          = [[blras.s3o]],
+          radarDistance       = 2400,
+	  seismicSignature    = 4,
+	  selfDestructAs      = [[BIG_UNITEX]],
+	  side                = [[NKG]],
+	  sightDistance       = 750,
+	  smoothAnim          = true,
+	  steeringmode        = [[1]],
+	  TEDClass            = [[SHIP]],
+	  turnRate            = 90,
+	  workerTime          = 0,
+          waterline           = 5,
+	  unitRestricted      = 2,
+          script              = [[blras.lua]],
+	
+	  sfxtypes            = {
+	
+	    explosiongenerators = {
+		"custom:orc_machinegun_flash",
+		"custom:orc_machinegun_muzzle",
+	    },
+	
+	  },
+
+
+
+	  weapons             = {
+	
+	    {
+	      def                = [[Cannon]],
+	      badTargetCategory  = [[SWIM LAND SHIP HOVER]],
+	      mainDir            = [[0 0 1]],
+	      maxAngleDif        = 180,
+	      onlyTargetCategory = [[SWIM LAND SINK FLOAT SHIP HOVER]],
+	    },
+	
+	  },
+	
+	
+	  weaponDefs          = {
+	
+	    Cannon = {
+	      name                    = [[Heavy Naval Artillery Cannon]],
+	      areaOfEffect            = 192,
+	      cegTag                  = [[vulcanfx]],
+	      craterMult              = 0.25,
+	
+	      damage                  = {
+		Cruiser = 1000,
+		Building = 2000,
+		Mech = 1000,
+		Tank = 1000,
+		Aircraft = 1000,
+		Ship = 1000,
+		Sub = 100,
+	      },
+	
+	      model                   = [[bshell.s3o]],
+	      explosionGenerator      = [[custom:lrpc_expl]],
+	      holdtime                = [[1]],
+	      interceptedByShieldType = 1,
+	      range                   = 2400,
+	      reloadtime              = 7,
+	      soundHit                = [[argh/Argh_MedExplosion]],
+	      soundStart              = [[argh/Argh_Whamsound]],
+	      turret                  = true,
+	      weaponType              = [[Cannon]],
+	      weaponVelocity          = 1100,
+	    },
+	
+	  },
+	
+	
+	  featureDefs         = {
+	
+	    DEAD  = {
+	      description      = [[Wreckage - Long Range Artillery Ship]],
+	      blocking         = false,
+	      category         = [[corpses]],
+	      damage           = 160,
+	      energy           = 0,
+	      featureDead      = [[DEAD2]],
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 2,
+	      footprintZ       = 2,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 15,
+	      object           = [[bgenericwreckage.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	
+	    DEAD2 = {
+	      description      = [[Debris - Long Range Artillery Ship]],
+	      blocking         = false,
+	      category         = [[heaps]],
+	      damage           = 160,
+	      energy           = 0,
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 8,
+	      footprintZ       = 8,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 15,
+	      object           = [[b8x8heap.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	  },
+	
+	}
+	
+	return lowerkeys({ blras = unitDef })
