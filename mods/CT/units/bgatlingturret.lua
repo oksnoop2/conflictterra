34d1e3b879a0b5dccb9f8e2361c27079d661e14f
sanadaujiosan@gmail.com <sanadaujiosan@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2010-07-04 23:30:55 +0000

    Adding missing unit.

---
+++ mods/CT/units/bgatlingturret.lua
@@ -0,0 +1,149 @@
+	unitDef = {
+	  unitname            = [[bgatlingturret]],
+	  name                = [[Gatling Turret]],
+	  description         = [[Rapid Fire Turret - Rapid fire cannon designed to take out several weaker units.]],
+	  acceleration        = 0,
+	  bmcode              = [[0]],
+	  brakeRate           = 0,
+	  buildCostEnergy     = 350,
+	  buildCostMetal      = 280,
+	  builder             = false,
+	  buildPic            = [[bgatlingturret.png]],
+	  buildTime           = 20,
+	  canAttack           = true,
+	  canstop             = [[1]],
+	  category            = [[SINK]],
+	  corpse              = [[DEAD]],
+          reclaimable         = false,
+	
+	  explodeAs           = [[SMALL_BUILDINGEX]],
+          footprintx          = 3,
+	  footprintZ          = 3,
+	  idleAutoHeal        = 0,
+	  levelGround         = false,
+	  mass                = 35,
+	  maxDamage           = 4500,
+          maxSlope            = 36,
+	  maxVelocity         = 0,
+          maxWaterDepth       = 0,
+	  minCloakDistance    = 150,
+	  noAutoFire          = false,
+	  noChaseCategory     = [[FIXEDWING LAND SHIP SATELLITE SWIM GUNSHIP SUB HOVER]],
+	  objectName          = [[bgatlingturret.s3o]],
+	  seismicSignature    = 4,
+	  selfDestructAs      = [[SMALL_BUILDINGEX]],
+	  side                = [[NKG]],
+	  sightDistance       = 400,
+
+	  sfxtypes            = {
+	
+	    explosiongenerators = {
+		"custom:orc_machinegun_flash",
+		"custom:orc_machinegun_muzzle",
+	    },
+	
+	  },
+
+	  smoothAnim          = true,
+	  TEDClass            = [[FORT]],
+	  turnRate            = 0,
+	  workerTime          = 0,
+	  yardMap             = [[ooo ooo ooo]],
+          script              = [[bgatlingturret.lua]],
+	
+	  weapons             = {
+	
+	    {
+	      def                = [[Machine_Gun]],
+	      onlyTargetCategory = [[LAND SINK SHIP SWIM FLOAT HOVER]],
+	    },
+	
+	  },
+	
+	
+	  weaponDefs          = {
+	
+	    Machine_Gun = {
+	      name                    = [[Rapid Fire Cannon]],
+	      areaOfEffect            = 8,
+	      craterMult              = 0.25,
+	
+	      damage                  = {
+		Cruiser = 30,
+		Building = 30,
+		Mech = 30,
+		Tank = 30,
+		Aircraft = 30,
+		Ship = 30,
+		Sub = 30,
+	      },
+	
+	      explosionGenerator      = [[custom:EMG_HIT]],
+	      interceptedByShieldType = 1,
+	      lineOfSight             = true,
+	      range                   = 300,
+	      reloadtime              = 0.2,
+	      rgbColor                = [[1 0.95 0.4]],
+	      separation              = 2,
+              size                    = 1.25,
+	      soundStart              = [[ct/machine_gun]],
+	      soundStartVolume        = 0.5,
+	      soundTrigger            = true,
+	      stages                  = 50,
+	      sprayAngle              = 1180,
+	      tolerance               = 8000,
+	      turret                  = true,
+	      weaponType              = [[Cannon]],
+	      weaponVelocity          = 750,
+	    },
+	
+	  },
+	
+	
+	  featureDefs         = {
+	
+	    DEAD  = {
+	      description      = [[Wreckage - Gatling Turret]],
+	      blocking         = false,
+	      category         = [[corpses]],
+	      damage           = 160,
+	      energy           = 0,
+	      featureDead      = [[DEAD2]],
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 2,
+	      footprintZ       = 2,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 15,
+	      object           = [[bgenericwreckage.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	
+	    DEAD2 = {
+	      description      = [[Debris - Gatling Turret]],
+	      blocking         = false,
+	      category         = [[heaps]],
+	      damage           = 160,
+	      energy           = 0,
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 3,
+	      footprintZ       = 3,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 15,
+	      object           = [[b3x3heap.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	  },
+	
+	}
+	
+	return lowerkeys({ bgatlingturret = unitDef })
