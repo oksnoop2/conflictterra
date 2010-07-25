62aae8ffd445aea405d749de7d5a88e58d58bc8b
sanadaujiosan@gmail.com <sanadaujiosan@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2010-07-25 21:18:04 +0000

    Redid Advanced Power Plant.  Used old Advanced Energy Storage model.  Now acts as a standard power plant, instead of a wind generator.

--- mods/CT/gamedata/sidedata.lua
+++ mods/CT/gamedata/sidedata.lua
@@ -23,7 +23,7 @@ local
 
     name      = 'TEST',
 
-    startunit = 'bmex',
+    startunit = 'bengineer2',
 
   },
 
--- mods/CT/scripts/badvpowerplant.lua
+++ mods/CT/scripts/badvpowerplant.lua
@@ -1,35 +1,6 @@
 local building = piece "building"
-local fins = piece "fins"
 
 
-local function startup_animation()
-	SetSignalMask(1)
-	Move (building, y_axis, 100, 50)
-	Spin (fins, x_axis, 2, 1)
-	WaitForMove (building, y_axis)
-	while true do
-		Move (building, z_axis, 15, 5)
-		WaitForMove (building, z_axis)
-		Sleep(300)
-		Move (building, z_axis, -15, 5)
-		WaitForMove (building, z_axis)
-		Sleep(300)
-	end
-end
-
-local function startup_stop()
-	Signal(1)
-	Move (building, y_axis, 0, 50)
-	StopSpin (fins, x_axis, 1)
-end
-
-function script.Activate()
-	StartThread(startup_animation)
-end
-
-function script.Deactivate()
-	StartThread(startup_stop)
-end
 
 function script.Killed(recentDamage, maxHealth)
 	return 0
