da503b0396505f8faf1db5c55005bca775905769
sanadaujiosan@gmail.com <sanadaujiosan@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2010-07-31 02:54:32 +0000

    Priming for Beta.

--- games/CT/modinfo.lua
+++ games/CT/modinfo.lua
@@ -1,7 +1,7 @@
 local modinfo = {
 	name = "Conflict Terra",
 	shortname = "ct",
-	version = "ver.08.4",
+	version = "ver.09",
 	game = "Conflict Terra",
 	shortgame = "ct",
 	description = "Conflict Terra",
