c813968693fadaa695337e1563259dcf8361617f
sanadaujiosan@gmail.com <sanadaujiosan@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2010-08-15 23:06:21 +0000

    Fixed pathfinding, using the largest footprint for each move type.

--- games/CT/gamedata/movedefs.lua
+++ games/CT/gamedata/movedefs.lua
@@ -11,109 +11,109 @@
 	local moveDefs = {
 
 	        Engineer = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 3,
+	                footprintz = 3,
 	                maxwaterdepth = 30,
 	                maxslope = 36,
 	                crushstrength = 50,
 	        },
 
 	        LightMech = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 4,
+	                footprintz = 4,
 	                maxwaterdepth = 22,
 	                maxslope = 36,
 	                crushstrength = 50,
 	        },
 	
 	        MedMech = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 4,
+	                footprintz = 4,
 	                maxwaterdepth = 22,
 	                maxslope = 36,
 	                crushstrength = 500,
 	        },
 
 	        HeavyMech = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 4,
+	                footprintz = 4,
 	                maxwaterdepth = 22,
 	                maxslope = 36,
 	                crushstrength = 5000,
 	        },
 
 	        LightTANK = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 2,
+	                footprintz = 2,
 	                maxwaterdepth = 22,
 	                maxslope = 18,
 	                crushstrength = 50,
 	        },
 
 	        MedTANK = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 4,
+	                footprintz = 4,
 	                maxwaterdepth = 22,
 	                maxslope = 18,
 	                crushstrength = 500,
 	        },
 
 	        HeavyTANK = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 5,
+	                footprintz = 5,
 	                maxwaterdepth = 22,
 	                maxslope = 18,
 	                crushstrength = 5000,
 	        },
 
 	        LightSHIP = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 6,
+	                footprintz = 6,
 	                minwaterdepth = 5,
 	                crushstrength = 50,
 	        },
 
 	        MedSHIP = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 5,
+	                footprintz = 5,
 	                minwaterdepth = 10,
 	                crushstrength = 500,
 	        },
 
 	        HeavySHIP = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 10,
+	                footprintz = 10,
 	                minwaterdepth = 15,
 	                crushstrength = 5000,
 	        },
 
 	        LightHOVER = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 2,
+	                footprintz = 2,
 	                maxwaterdepth = 22,
 	                maxslope = 18,
 	                crushstrength = 50,
 	        },
 
 	        MedHOVER = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 4,
+	                footprintz = 4,
 	                maxwaterdepth = 22,
 	                maxslope = 18,
 	                crushstrength = 500,
 	        },
 
 	        HeavyHOVER = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 10,
+	                footprintz = 10,
 	                maxwaterdepth = 22,
 	                maxslope = 18,
 	                crushstrength = 5000,
 	        },
 
 	        SubBOAT = {
-	                footprintx = 1,
-	                footprintz = 1,
+	                footprintx = 5,
+	                footprintz = 5,
 	                minwaterdepth = 45,
 	                crushstrength = 500,
 	                subMarine = 1,
