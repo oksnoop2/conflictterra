c1abe0eaef4a8d5e5fa9300f90f2d3c9a638c0c8
anmeldekram71@web.de <anmeldekram71@web.de@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2010-10-26 15:00:35 +0000

    -a ugly WIP trail for the meteors image: http://h-1.abload.de/img/omgitsafuckingrockq6bx.png

---
+++ games/CT/gamedata/explosions/firetrail.lua
@@ -0,0 +1,38 @@
+--digdig changed to look like firetrail of a meteor by knorke
+--wow this looks ugly
+return {
+  ["fire"] = {
+    dirt = {
+      air                = true,
+      count              = 2,
+      ground             = true,
+      properties = {
+        color              = [[.3, .15, .02]],        
+		lifetime           = 10,
+        pos                = [[0, 0, 0]],
+        size               = 80,
+        sizeexpansion      = 10,
+        slowdown           = .1,
+        speed              = [[0.0025 r-0.6, 1 r1.0, 0.0025 r-0.6]],
+		alwaysVisible = 1,
+	  },
+    },
+  },
+
+  ["firetrail"] = {
+    burst = {
+      air                = true,
+      class              = [[CExpGenSpawner]],
+      count              = 2,
+      ground             = true,
+      water              = true,
+      properties = {
+        delay              = [[0 i5]],
+        explosiongenerator = [[custom:fire]],
+        pos                = [[0, 0, 0]],
+      },
+    },
+  },
+
+}
+
