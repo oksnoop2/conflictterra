2b003081dca91dcd16ae574cdea0d45e2ddb9b80
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2010-11-03 22:10:34 +0000

    some fixes for the replicator

--- games/CT/scripts/kdrone.lua
+++ games/CT/scripts/kdrone.lua
@@ -27,7 +27,41 @@
 	local SIG_AIM_SEC = 2
 	local walk_go = 4
 	local walk_stop = 8
+local SIG_AIM = 2
+local SIG_BUILD = 4
 
+function script.Create(unitID)
+end
+
+function script.StartBuilding(heading, pitch)
+	Signal(SIG_BUILD)
+	SetSignalMask(SIG_BUILD)
+        Turn(body, y_axis, heading, math.rad(90))
+        WaitForTurn(body, y_axis)
+	SetUnitValue(COB.INBUILDSTANCE, 1)
+	return 1
+end
+function script.Querybody() return flare1 end
+function script.AimFromWeapon() return body end
+
+function script.StopBuilding()
+   Signal(SIG_BUILD)
+   SetSignalMask(SIG_BUILD)
+   SetUnitValue(COB.INBUILDSTANCE, 0)
+   Turn(body, y_axis, 0, math.rad(90))
+   WaitForTurn(body, y_axis)
+   Sleep(1)
+   return 0
+end
+function script.AimFromWeapon() return flare1 end
+
+function script.QueryWeapon() return flare1 end
+
+function script.Queryflare1Piece() return flare1 end
+
+function script.Killed(recentDamage, maxHealth)
+   return 0
+end
 	
 	function script.Create()
 	       
