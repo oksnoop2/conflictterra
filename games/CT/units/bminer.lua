b4d76187993799efb4d57f419418ba1dd5b23ef9
sanadaujiosan@gmail.com <sanadaujiosan@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2010-11-19 03:27:19 +0000

    - Removed texture and model for an old, old model that won't be used. - Reduced miner's building damage to 1, made as accurate as possible and increased velocity. - Cleaned up minerals unit def and little.

--- games/CT/units/bminer.lua
+++ games/CT/units/bminer.lua
@@ -38,6 +38,11 @@ local bminer = {
         corpse              = [[DEAD]],
         reclaimable         = false,
 
+	customParams = {
+		is_miner=1,	--used by minig: if the unit can mine is_mineable=1 ressources
+		max_cargo=25,	--used by mining: how much metal the unit can carry at once before having to return to a drop off
+	},
+
         defaultmissiontype  = [[Standby]],
         energyMake          = 0.25,
         explodeAs           = [[SMALL_UNIT]],
@@ -95,10 +100,7 @@ local bminer = {
         showNanospray       = 0,
         workerTime          = 1,
         script              = "bminer.lua",
-		customParams = {
-		is_miner=1,		--used by minig: if the unit can mine is_mineable=1 ressources
-		max_cargo=25,	--used by mining: how much metal the unit can carry at once before having to return to a drop off
-		},
+
           weapons             = {
         
             {
@@ -116,13 +118,14 @@ local bminer = {
             Rock_Drill = {
               name                    = [[Rock Drill]],
               areaOfEffect            = 8,
+              avoidFriendly           = 0,
               craterMult              = 0.25,
-              accuracy                = 200,
+              accuracy                = 0,
 	      collideFriendly         = false,
         
               damage                  = {
                 Cruiser = 1,
-                Building = 15,
+                Building = 1,
                 Mech = 1,
                 Tank = 1,
                 Aircraft = 1,
@@ -130,25 +133,25 @@ local bminer = {
                 Sub = 1,
               },
         
-              explosionGenerator      = [[custom:resmining]], -- [[custom:FLAK_BURST_Expl]],
+              explosionGenerator      = [[custom:resmining]],
               interceptedByShieldType = 1,
               lineOfSight             = true,
               range                   = 90,
               reloadtime              = 0.2,
               rgbColor                = [[1 0.95 0.4]],
               separation              = 2,
-              size                    = 0.4, --was 1.25
-              soundStart              = [[ct/machine_gun]],
+              size                    = 0.4,
+            --  soundStart              = [[ct/machine_gun]],
               soundStartVolume        = 0.5,
               soundTrigger            = true,
               stages                  = 50,
-              sprayAngle              = 1180,
+              targetBorder            = 1,
               tolerance               = 8000,
               turret                  = true,
               weaponType              = [[Cannon]],
-              weaponVelocity          = 750,
-              targetBorder=1,
-              avoidFriendly=0,
+              weaponVelocity          = 7500,
+
+
 			},
         
           },
--- games/CT/units/bminerals.lua
+++ games/CT/units/bminerals.lua
@@ -10,12 +10,12 @@ unitDef = {
   buildCostEnergy               = 1,
   buildCostMetal                = 1,
   builder                       = false,
-  iconType           			= "resource",
+  iconType           		= "resource",
   buildPic                      = [[bmex.png]],
   buildTime                     = 1,
   canattack                     = [[false]],
   category                      = [[MINERALS]],
---  corpse                        = [[DEAD]],
+
   reclaimable                   = false,
   repairable			= false,
   energyUse                     = 0,
@@ -24,6 +24,7 @@ unitDef = {
   floater                       = false,
   footprintX                    = 3,
   footprintZ                    = 3,
+--  hideDamage                    = true,
   idleAutoHeal                  = 0,
   levelGround                   = false,
   mass                          = 37.5,
@@ -70,59 +71,6 @@ unitDef = {
     customParams = {
   	is_mineable=1,		--used by minig: if the unit is a mineable resource
 	},
-
-
-
-  featureDefs                   = {
-
-    DEAD  = {
-      description      = [[this mineral is mined out.]],
-      blocking         = false,
-      category         = [[heaps]],
-      damage           = 10,
-      energy           = 0,
-      featureDead      = [[DEAD2]],
-      featurereclamate = [[SMUDGE01]],
-      footprintX       = 2,
-      footprintZ       = 2,
-      height           = [[40]],
-      hitdensity       = [[100]],
-      metal            = 30,
-      object           = [[b3x3heap.s3o]],
-      reclaimable      = true,
-      reclaimTime      = 275,
-
-      seqnamereclamate = [[TREE1RECLAMATE]],
-      world            = [[All Worlds]],
-    },
-
-
-    DEAD2 = {
-      description      = [[this mined mineral is totally crushed dude!]],
-      blocking         = false,
-
-      category         = [[heaps]],
-      damage           = 2250,
-      energy           = 0,
-      featurereclamate = [[SMUDGE01]],
-      footprintX       = 3,
-      footprintZ       = 3,
-      height           = [[4]],
-      hitdensity       = [[100]],
-      metal            = 15,
-      object           = [[b3x3heap.s3o]],
-      reclaimable      = true,
-      reclaimTime      = 275,
-      seqnamereclamate = [[TREE1RECLAMATE]],
-      world            = [[All Worlds]],
-
-    },
-
-
-  },
-
-
-
 }
 
 
