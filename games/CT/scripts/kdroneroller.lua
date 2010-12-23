4624ee2675a449220051fbcb8da7b40f65cb9235
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2010-12-23 04:04:19 +0000

    Roller Drone, Textured and with impulse now just needs a script and a gadget

---
+++ games/CT/scripts/kdroneroller.lua
@@ -0,0 +1,67 @@
+	-- by KR
+	
+	--pieces
+	local body = piece "body"
+	local flare1 = piece "flare1"
+
+	--signals
+	local SIG_AIM = 1
+	local SIG_BUILD = 2
+
+	function script.Create(unitID)
+	end
+
+	function script.StartBuilding(heading, pitch)
+		Signal(SIG_BUILD)
+		SetSignalMask(SIG_BUILD)
+        	Turn(body, y_axis, heading, math.rad(90))
+        	WaitForTurn(body, y_axis)
+		SetUnitValue(COB.INBUILDSTANCE, 1)
+		return 1
+	end
+
+	function script.Querybody() return flare1 end
+
+	function script.AimFromWeapon() return body end
+
+	function script.StopBuilding()
+		Signal(SIG_BUILD)
+		SetSignalMask(SIG_BUILD)
+		SetUnitValue(COB.INBUILDSTANCE, 0)
+		Turn(body, y_axis, 0, math.rad(90))
+		WaitForTurn(body, y_axis)
+		Sleep(1)
+		return 0
+	end
+
+	function script.AimFromWeapon() return flare1 end
+
+	function script.QueryWeapon() return flare1 end
+
+	function script.QueryNanoPiece() return flare1 end
+	
+	local function RestoreAfterDelay(unitID)
+		Sleep(2500)
+		Turn(body, y_axis, 0, math.rad(150))
+	end
+
+	function script.QueryWeapon1() return flare1 end
+	
+	function script.AimFromWeapon1() return body end
+	
+	function script.AimWeapon1( heading, pitch )
+                Signal(SIG_AIM)
+		SetSignalMask(SIG_AIM)
+        	Turn(body, y_axis, heading, math.rad(150))
+        	WaitForTurn(body, y_axis)
+		StartThread(RestoreAfterDelay)
+		return true
+	end
+	
+	function script.FireWeapon1()
+	       Sleep(30)
+	end
+	
+	function script.Killed(recentDamage, maxHealth)
+		Sleep(30)
+	end
---
+++ games/CT/units/kdroneroller.lua
@@ -0,0 +1,168 @@
+	unitDef = {
+	  unitname            = [[kdroneroller]],
+	  name                = [[Roller Drone]],
+	  description         = [[It drones, it rolls, it's rock and lolz?]],
+	  acceleration        = 0.04,
+	  bmcode              = [[1]],
+	  brakeRate           = 0.02,
+	  buildCostEnergy     = 600,
+	  buildCostMetal      = 600,
+	  builder             = false,
+	  buildPic            = [[kdroneroller.png]],
+	  buildTime           = 10,
+	  canAttack           = true,
+	  canGuard            = true,
+	  canMove             = true,
+	  canPatrol           = true,
+	  canstop             = [[1]],
+	  category            = [[LAND]],
+	  corpse              = [[DEAD]],
+          reclaimable         = false,
+	
+	  defaultmissiontype  = [[Standby]],
+	  explodeAs           = [[SMALL_UNIT]],
+          footprintx          = 2,
+	  footprintZ          = 2,
+	  idleAutoHeal        = 0,
+	  leaveTracks         = true,
+	  maneuverleashlength = [[640]],
+	  mass                = 90,
+	  maxDamage           = 1150,
+          maxSlope            = 18,
+	  maxVelocity         = 1.5,
+          maxWaterDepth       = 22,
+	  minCloakDistance    = 75,
+	  movementClass       = [[LightTANK]],
+	  moveState           = 0,
+	  noAutoFire          = false,
+	  noChaseCategory     = [[MINERALS FIXEDWING GUNSHIP SUB]],
+	  objectName          = [[kdroneroller.s3o]],
+	  seismicSignature    = 4,
+	  selfDestructAs      = [[SMALL_UNIT]],
+
+	  	    
+	  sfxtypes            = {
+	
+	    explosiongenerators = {
+		"custom:orc_machinegun_flash",
+		"custom:orc_machinegun_muzzle",
+	    },
+	
+	  },
+
+	  sounds			= {
+	      select = {
+		"argh/Argh_TankSound",
+		},
+	      ok = {
+		"argh/Argh_TankSound",
+		},
+	  },
+
+	  side                = [[Drone]],
+	  sightDistance       = 340,
+	  smoothAnim          = true,
+	  steeringmode        = [[1]],
+	  TEDClass            = [[TANK]],
+	  trackOffset         = 3,
+	  trackStrength       = 6,
+	  trackStretch        = 1,
+	  trackType           = [[StdTank]],
+	  trackWidth          = 24,
+	  turninplace         = 0,
+	  turnRate            = 550,
+	  workerTime          = 0,
+          script              = [[kdroneroller.lua]],
+	
+	  weapons             = {
+	
+	    {
+	      def                = [[Cannon]],
+	      badTargetCategory  = [[SINK FLOAT]],
+	      onlyTargetCategory = [[LAND SINK SHIP SWIM FLOAT HOVER]],
+	    },
+	
+	  },
+	
+	
+	  weaponDefs          = {
+	
+	    Cannon = {
+	      name                    = [[Light Cannon]],
+	      areaOfEffect            = 500,
+	      craterMult              = 0.25,
+	
+	      damage                  = {
+		Cruiser = 100,
+		Building = 100,
+		Mech = 100,
+		Tank = 100,
+		Aircraft = 100,
+		Ship = 100,
+		Sub = 100,
+	      },
+	
+	      model                   = [[bshell.s3o]],
+	      explosionGenerator      = [[custom:PLASMA_Expl]],
+	      interceptedByShieldType = 1,
+          impulseBoost            = 10,
+          impulseFactor           = 15,	      
+		  range                   = 340,
+	      reloadtime              = 3,
+	      soundHit                = [[argh/Argh_MedExplosion]],
+	      soundStart              = [[argh/Argh_heavy_machinegun]],
+	      turret                  = true,
+	      weaponType              = [[Cannon]],
+	      weaponVelocity          = 600,
+	    },
+	
+	  },
+	
+	
+	  featureDefs         = {
+	
+	    DEAD  = {
+	      description      = [[Wreckage - Assault Tank]],
+	      blocking         = false,
+	      category         = [[corpses]],
+	      damage           = 1125,
+	      energy           = 0,
+	      featureDead      = [[DEAD2]],
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 2,
+	      footprintZ       = 2,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 72,
+	      object           = [[bgenericwreckage.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	
+	    DEAD2 = {
+	      description      = [[Debris - Assault Tank]],
+	      blocking         = false,
+	      category         = [[heaps]],
+	      damage           = 750,
+	      energy           = 0,
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 2,
+	      footprintZ       = 2,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 36,
+	      object           = [[b2x2heap.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	  },
+	
+	}
+	
+	return lowerkeys({ kdroneroller = unitDef })
