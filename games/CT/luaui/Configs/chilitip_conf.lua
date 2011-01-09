9dff0c31436207a048cc15b0b881403252e3bb75
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-01-09 22:03:47 +0000

    Adding chili, it works, but it has sooo much ugly teal. It's not on by default,

---
+++ games/CT/luaui/Configs/chilitip_conf.lua
@@ -0,0 +1,23 @@
+local ignoreweapon =
+{
+	armaak = {1},
+	armcarry = {1},
+	armaas = {1},
+	armraz = {2},
+	
+	cormak = {1,3},
+	coraak = {1},
+	coramph = {2},
+
+	armcom = {1,2,3},
+	armadvcom = {1,2},	
+	corcom = {1,2,3},
+	coradvcom = {1},
+	commadvrecon = {2},
+	commadvsupport = {2},
+	
+}
+local iconFormat = '.dds'
+
+return ignoreweapon, iconFormat
+
