232bfb279ee6d9c75418ffea045ba00e36ace5d1
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-01-09 08:49:18 +0000

    added some {{}} as per knorkes instructions

--- games/CT/LuaRules/Gadgets/tp_mining.lua
+++ games/CT/LuaRules/Gadgets/tp_mining.lua
@@ -19,7 +19,7 @@ local dropoffs = {} 	-- [unitID]
 local resources = {} 	-- [unitID] frame_last_mined
 local debug = false
 -----config-----
-local miner_name = "bminer", "kdroneminer";			--the unit used for mining
+local miner_name = {}-- {"bminer", "kdroneminer";}			--the unit used for mining
 local resource_name = {}--{"bminerals","bmeteorimpact","bmeteorimpact_big"}		--the stuff that gets mined
 local dropoff_name = {"bsupplydepot", "bsupplydepotai", "bflagshipbase2", "bflagshipbaseai","kdronestructure"}	--where the miners bring the resources to
 local dropoff_distance = 100 --how near do miners have to get to a dropoff to drop their cargo? (this value is added to unitRadius)
