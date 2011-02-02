b194e5f1da6cc82dc25c3a9b80effd083c43e96a
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-02-02 22:20:12 +0000

    drone mining now cheaper, gives radar and you get free flying drone on death

---
+++ games/CT/LuaRules/Gadgets/unit_kdroneminingtower_revenge.lua
@@ -0,0 +1,32 @@
+function gadget:GetInfo()
+	return {
+		name = "magella's last resort",
+		desc = "Spawns magella top on magella death",
+		author = "Smoth, Lurker",
+		date = "3-9-10",
+		license = "Public Domain",
+		layer = 1,
+		enabled = true
+	}
+end
+
+local magellaReplaceDefs
+	
+if (gadgetHandler:IsSyncedCode()) then
+
+  
+	function gadget:Initialize()
+		magellaReplaceDefs = {} for _,udname in pairs({"kdroneminingtower"}) do magellaReplaceDefs[UnitDefNames[udname].id] = true end
+	end
+	
+	function gadget:UnitDestroyed(u,ud,team)
+	
+		if (Spring.GetUnitHealth(u) < 0 ) then
+			if  magellaReplaceDefs[ud] then
+				local x,y,z = Spring.GetUnitBasePosition(u) 
+				
+				newGuy 	= Spring.CreateUnit("kairdrone", x, y, z, 0, team)
+			end
+		end
+	end
+end
