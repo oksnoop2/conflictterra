6c000be27f2635872bb139f6c13cac3a56a72c9b
sanadaujiosan@gmail.com <sanadaujiosan@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-02-03 01:22:56 +0000

    - Energy storage removed from game.  1000 is now the only amount of energy storage available, and isn't unit controlled, aka it's always 1000. - Removed support factory (as supplying storage was it's only purpose.

--- games/CT/units/bminer.lua
+++ games/CT/units/bminer.lua
@@ -13,7 +13,6 @@ local bminer = {
 		[[bsupplydepot]],
                 [[bpowerplant]],
                 [[bnuclearpower]],
-                [[bsupportfactory]],
 		[[bbeacon]],
 --                [[bshipyard]],
 --                [[bsubpens]],
