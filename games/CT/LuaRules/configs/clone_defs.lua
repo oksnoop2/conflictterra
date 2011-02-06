06dba7f564ce9f5376087202866ceaaec36f237b
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-02-06 04:27:58 +0000

    updates

--- games/CT/LuaRules/configs/clone_defs.lua
+++ games/CT/LuaRules/configs/clone_defs.lua
@@ -8,38 +8,38 @@ local cloneDefs = {
     into = 'kdroneengineer',
     tech = 0,
     time = 30,
-    metal = 150,
-    energy = 150,
+    metal = 100,
+    energy = 100,
   },
   kdronewarrior = {
     into = 'kdronewarrior',
     tech = 0,
     time = 30,
-    metal = 600,
-    energy = 600,
+    metal = 300,
+    energy = 300,
   },
   kairdrone = {
     into = 'kairdrone',
     tech = 0,
     time = 30,
-    metal = 300,
-    energy = 300,
+    metal = 100,
+    energy = 100,
   },
   
   kdroneroller = {
     into = 'kdroneroller',
     tech = 0,
     time = 30,
-    metal = 750,
-    energy = 750,
+    metal = 250,
+    energy = 250,
   },
 
     kdroneminer = {
     into = 'kdroneminer',
     tech = 0,
     time = 30,
-    metal = 750,
-    energy = 750,
+    metal = 250,
+    energy = 250,
   },
 }
 
