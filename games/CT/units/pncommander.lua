99d246d9ee800b7c08aa3d13d2b6e9ecf0ce9cc1
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-02-13 19:58:49 +0000

    Need scripts and other things
    PN stuff

---
+++ games/CT/units/pncommander.lua
@@ -0,0 +1,185 @@
+local pncommander = {
+	name                = [[PN Commander]],
+	description         = [[Commanding, is'nt he?]],
+    acceleration        = 0.06,
+    brakerate           = 1.5,
+    buildpic            = [[pncommander.png]],
+	buildCostEnergy     = 100,
+	buildCostMetal      = 100,
+    buildDistance       = 150,
+    builder             = true,
+	buildoptions		= {
+        
+ 		[[kdronestructure]],
+		[[kdroneminingtower]],
+		
+
+               
+	},
+
+	buildTime           = 15,
+    canAssist           = true,
+	CanAttack 			= 1,
+	CanCapture			= false,
+    canGuard            = true,
+	canMove		  	    = true,
+	canPatrol           = true,
+    canReclaim          = true,
+	canstop             = [[1]],
+	category            = [[LAND]],
+    corpse              = [[DEAD]],
+    reclaimable         = false,
+    defaultmissiontype  = [[Standby]],
+    energyMake          = 1,
+	explodeAs           = [[SMALL_UNIT]],
+    footprintx          = 2,
+	footprintZ          = 2,
+	idleAutoHeal        = 1,
+	leaveTracks         = false,
+	maneuverleashlength = [[640]],
+	mass                = 80,
+	maxDamage           = 500,
+    maxSlope            = 99999999,
+	maxVelocity         = 4,
+    maxWaterDepth       = 10000,
+    metalMake           = 0,
+	minCloakDistance    = 75,
+	movementClass       = [[kdrone]],
+    noChaseCategory     = [[MINERALS]],
+	objectName          = "pncommander.s3o",
+    onoffable           = false,
+    seismicSignature    = 4,
+    selfDestructAs      = [[SMALL_UNIT]],
+    side                = [[Drone]],
+    sightDistance       = 300,
+	sounds			= {
+	      select = {
+		"golgotha/vehicle_done_44khz",
+		},
+	      ok = {
+		"golgotha/vehicle_done_44khz",
+		},
+	  },
+
+	sfxtypes            = {
+	
+	    explosiongenerators = {
+		"custom:buildersparks",
+	    },
+	
+	  },
+
+	weapons             = {
+	
+	    {
+	      def                = [[Laser]],
+	      badTargetCategory  = [[GUNSHIP SINK FLOAT]],
+	      onlyTargetCategory = [[SWIM LAND SHIP SINK FLOAT GUNSHIP HOVER]],
+	    },
+	
+	  },
+	
+	
+	weaponDefs             = {
+	
+	    Laser = {
+	      name                    = [[Burst Laser]],
+	      areaOfEffect            = 8,
+	      coreThickness           = 0.5,
+	      collideFriendly         = false,
+	      craterMult              = 0.25,
+	
+	      damage                  = {
+		Cruiser = 10,
+		Building = 10,
+		Mech = 10,
+		Tank = 10,
+		Aircraft = 10,
+		Ship = 10,
+		Sub = 10,
+	      },
+	
+	      duration                = 0.02,
+	      energypershot           = 0,
+	      interceptedByShieldType = 1,
+	      heightMod               = 0.5,
+	      lineOfSight             = true,
+	      range                   = 300,
+	      reloadtime              = 0.5,
+	      rgbColor                = [[0 1 0]],
+	      soundStart              = [[ct/pew1]],
+	      targetMoveError         = 0.15,
+	      thickness               = 1,
+	      tolerance               = 10000,
+	      turret                  = true,
+	      weaponType              = [[LaserCannon]],
+	      weaponVelocity          = 1500,
+	    },
+	
+	  },
+	  
+	smoothAnim          = true,
+	steeringmode        = [[1]],
+    terraformSpeed      = 300,
+	reclaimSpeed        = 300,
+	trackOffset         = 12,
+	trackStrength       = 5,
+	trackStretch        = 1,
+	trackType           = [[StdTank]],
+	trackWidth          = 15,
+	turninplace         = 0,
+	turnInPlace         = 0,
+	turnRate            = 560,
+	showNanospray	    = 1,
+	Resurrectspeed      =100,
+	workerTime          = 1,
+	--script		    = "pncommander.lua",
+
+	featureDefs         = {
+	
+	    DEAD  = {
+	      description      = [[Wreckage - PN Commander]],
+	      blocking         = true,
+	      category         = [[corpses]],
+	      damage           = 262,
+	      energy           = 0,
+	      featureDead      = [[DEAD2]],
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 3,
+	      footprintZ       = 3,
+	      height           = [[20]],
+	      hitdensity       = [[100]],
+	      metal            = 50,
+	      object           = [[bgenericwreckage.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 70,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	
+	    DEAD2 = {
+	      description      = [[Debris - PN Commander]],
+	      blocking         = false,
+	      category         = [[heaps]],
+	      damage           = 175,
+	      energy           = 0,
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 2,
+	      footprintZ       = 2,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 25,
+	      object           = [[b2x2heap.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 70,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+
+  	  },
+
+	}
+
+
+return lowerkeys({ ["pncommander"] = pncommander })
---
+++ games/CT/units/pnfieldradar.lua
@@ -0,0 +1,162 @@
+	unitDef = {
+	  unitname            = [[pnfieldradar]],
+	  name                = [[Radar]],
+	  description         = [[raaaaaaadical radar]],
+	  acceleration        = 0,
+	  bmcode              = [[0]],
+	  brakeRate           = 0,
+	  buildCostEnergy     = 100,
+	  buildCostMetal      = 100,
+	  builder             = false,
+	  buildPic            = [[pnfieldradar.png]],
+	  buildTime           = 30,
+	  canAttack           = true,
+	  canstop             = [[1]],
+	  category            = [[SINK]],
+	  corpse              = [[DEAD]],
+	  reclaimable         = false,
+	  radarDistance       =  300,
+	  damageModifier      = 0.25,
+	  --defaultmissiontype  = [[GUARD_NOMOVE]],
+	  explodeAs           = [[SMALL_BUILDING]],
+	  fireState           = 2,
+          footprintx          = 2,
+	  footprintZ          = 2,
+	  idleAutoHeal        = 0,
+	  levelGround         = false,
+	  mass                = 1300,
+	  maxDamage           = 1000,
+          maxSlope            = 18,
+	  maxVelocity         = 0,
+          maxWaterDepth       = 0,
+	  minCloakDistance    = 150,
+	  noAutoFire          = false,
+	  noChaseCategory     = [[MINERALS FIXEDWING GUNSHIP SUB]],
+	  objectName          = [[pnfieldradar.s3o]],
+	  seismicSignature    = 4,
+	  selfDestructAs      = [[SMALL_BUILDING]],
+	  side                = [[NKG]],
+	  sightDistance       = 450,
+
+	  sounds			= {
+	      select = {
+		"evolutionRTS/turretselect",
+		},
+
+	      ok = {
+		"evolutionRTS/turretselect",
+		},
+	  },
+
+	  smoothAnim          = true,
+	  turnRate            = 0,
+	  workerTime          = 0,
+	  yardMap             = [[ooooo ooooo ooooo ooooo ooooo]],
+          --script              = [[pnfieldradar.lua]],
+	
+	  weapons             = {
+        
+            {
+              def                = [[Rock_Drill]],
+              onlyTargetCategory = [[MINERALS]],
+            },
+        
+          },
+	
+	
+	            weaponDefs          = {
+        
+            Rock_Drill = {
+              name                    = [[Rock Drill]],
+              areaOfEffect            = 8,
+              avoidFriendly           = 0,
+	      beamTime                = 0.2,
+              craterMult              = 0.25,
+              accuracy                = 0,
+	      collideFriendly         = false,
+	      energypershot           = 0,
+        
+              damage                  = {
+                Cruiser = 1,
+                Building = 1,
+                Mech = 1,
+                Tank = 1,
+                Aircraft = 1,
+                Ship = 1,
+                Sub = 1,
+              },
+        
+              explosionGenerator      = [[custom:resmining]],
+              interceptedByShieldType = 1,
+              lineOfSight             = true,
+              range                   = 130,
+              reloadtime              = 0.2,
+              rgbColor                = [[0 1 0]],
+              separation              = 2,
+	      thickness               = 1,
+	      minIntensity            = 1,
+              size                    = 0.4,
+              soundStart              = [[tp/swoosh]],
+              soundStartVolume        = 0.5,
+              soundTrigger            = true,
+              stages                  = 50,
+              targetBorder            = 1,
+              tolerance               = 8000,
+              turret                  = true,
+              weaponType              = [[BeamLaser]],
+              weaponVelocity          = 2500,
+
+
+			},
+        
+          },
+	
+	
+	  featureDefs         = {
+	
+	    DEAD  = {
+	      description      = [[Wreckage - Mining Tower]],
+	      blocking         = false,
+	      category         = [[corpses]],
+	      damage           = 8625,
+	      energy           = 0,
+		  
+	      featureDead      = [[DEAD2]],
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 2,
+	      footprintZ       = 2,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 400,
+	      object           = [[bgenericwreckage.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	
+	    DEAD2 = {
+	      description      = [[Debris - Mining Tower]],
+	      blocking         = false,
+	      category         = [[heaps]],
+	      damage           = 5750,
+	      energy           = 0,
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 5,
+	      footprintZ       = 5,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 200,
+	      object           = [[b5x5heap.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	  },
+	
+	}
+	
+	return lowerkeys({ pnfieldradar = unitDef })
