6aeb99f6ccf4e89f92cad3b09721ed83885bcca5
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-03-12 22:45:07 +0000

    pnaamech says "script me so i can shoot your planes"

---
+++ games/CT/units/pnaamech.lua
@@ -0,0 +1,206 @@
+	unitDef = {
+	  unitname            = [[pnaamech]],
+	  name                = [[AA Mech]],
+	  description         = [[The anti aircraft walker.]],
+	  acceleration        = 0.12,
+	  brakeRate           = 0.225,
+	  buildCostEnergy     = 375,
+	  buildCostMetal      = 375,
+	  builder             = false,
+	  buildPic            = [[pnaamech.png]],
+	  buildTime           = 20,
+	  canAttack           = true,
+	  canGuard            = true,
+	  canMove             = true,
+	  canPatrol           = true,
+	  canStop             = true,
+	  category            = [[LAND]],
+	  corpse              = [[DEAD]],
+          reclaimable         = false,
+	
+	  explodeAs           = [[MEDIUM_UNIT]],
+	  footprintX          = 3,
+	  footprintZ          = 3,
+	  idleAutoHeal        = 0,
+	  mass                = 65,
+	  maxDamage           = 750,
+	  maxSlope            = 36,
+	  maxVelocity         = 3,
+	  maxWaterDepth       = 22,
+	  minCloakDistance    = 75,
+	  movementClass       = [[Mech3x3]],
+	  moveState           = 0,
+	  noAutoFire          = false,
+	  noChaseCategory     = [[MINERALS FIXEDWING GUNSHIP SUB]],
+	  objectName          = [[pnaamech.s3o]],
+	  seismicSignature    = 4,
+	  selfDestructAs      = [[BIG_UNITEX]],
+
+	  sfxtypes            = {
+	
+	    explosiongenerators = {
+		"custom:orc_machinegun_flash",
+		"custom:orc_machinegun_muzzle",
+	    },
+	
+	  },
+
+
+
+	  sounds			= {
+	      select = {
+		"golgotha/vehicle_done_44khz",
+		},
+	      ok = {
+		"golgotha/vehicle_done_44khz",
+		},
+	  },
+
+	  side                = [[NKG]],
+	  sightDistance       = 750,
+	  smoothAnim          = true,
+	  turnInPlace         = 0,
+	  turnRate            = 500,
+	  upright             = true,
+	  workerTime          = 0,
+      ----script              = [[pnaamech.lua]],
+	
+	  weapons             = {
+
+	    {
+	      def                = [[Cannon]],
+	      badTargetCategory  = [[SWIM LAND SHIP HOVER]],
+	      mainDir            = [[0 0 1]],
+	      maxAngleDif        = 180,
+	      onlyTargetCategory = [[SWIM LAND SINK FLOAT SHIP HOVER]],
+	    },
+	
+	    {
+	      def                = [[Machine_Gun]],
+	      badTargetCategory  = [[SINK FLOAT]],
+	      mainDir            = [[0 0 1]],
+	      maxAngleDif        = 180,
+	      onlyTargetCategory = [[LAND SINK SHIP SWIM FLOAT HOVER]],
+	    },
+	
+	  },
+	
+	
+	  weaponDefs          = {
+	
+	    Cannon = {
+	      name                    = [[Artillery Cannon]],
+	      areaOfEffect            = 64,
+              craterMult              = 0.25,
+	      accuracy                = 500,
+	
+	      damage                  = {
+		Cruiser = 100,
+		Building = 500,
+		Mech = 100,
+		Tank = 100,
+		Aircraft = 100,
+		Ship = 100,
+		Sub = 100,
+	      },
+	
+	      model                   = [[bshell.s3o]],
+	      explosionGenerator      = [[custom:LRPC_Expl]],
+	      interceptedByShieldType = 1,
+	      myGravity               = 0.1,
+	      range                   = 750,
+	      reloadtime              = 4.5,
+	      soundHit                = [[argh/Argh_MedExplosion]],
+	      soundStart              = [[argh/Argh_Whamsound]],
+	      turret                  = true,
+	      weaponType              = [[Cannon]],
+	      weaponVelocity          = 750,
+	    },
+
+	    Machine_Gun = {
+	      name                    = [[Rapid Fire Cannon]],
+	      areaOfEffect            = 8,
+	      burst                   = 2,
+	      burstrate               = 0.001,
+	      craterMult              = 0.25,
+	      accuracy                = 200,
+	
+	      damage                  = {
+		Cruiser = 15,
+		Building = 15,
+		Mech = 15,
+		Tank = 15,
+		Aircraft = 15,
+		Ship = 15,
+		Sub = 15,
+	      },
+	
+	      explosionGenerator      = [[custom:FLAK_BURST_Expl]],
+	      interceptedByShieldType = 1,
+	      lineOfSight             = true,
+	      range                   = 200,
+	      reloadtime              = 0.2,
+	      rgbColor                = [[1 0.95 0.4]],
+	      separation              = 2,
+              size                    = 1.25,
+	      soundStart              = [[ct/machine_gun]],
+	      soundStartVolume        = 0.5,
+	      soundTrigger            = true,
+	      stages                  = 50,
+	      sprayAngle              = 1180,
+	      tolerance               = 8000,
+	      turret                  = true,
+	      weaponType              = [[Cannon]],
+	      weaponVelocity          = 750,
+	    },
+	
+	  },
+	
+	
+	  featureDefs         = {
+	
+	    DEAD  = {
+	      description      = [[Wreckage - Jump Mech]],
+	      blocking         = false,
+	      category         = [[corpses]],
+	      damage           = 562,
+	      energy           = 0,
+	      featureDead      = [[DEAD2]],
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 2,
+	      footprintZ       = 2,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 200,
+	      object           = [[bgenericwreckage.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	
+	    DEAD2 = {
+	      description      = [[Debris - Jump Mech]],
+	      blocking         = false,
+	      category         = [[heaps]],
+	      damage           = 375,
+	      energy           = 0,
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 3,
+	      footprintZ       = 3,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 100,
+	      object           = [[b3x3heap.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	  },
+	
+	}
+	
+	return lowerkeys({ pnaamech = unitDef })
