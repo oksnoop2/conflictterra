e4d1c3b62509b32f74a6ba154157b86d57fe9129
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-03-20 02:01:41 +0000

    adding pn artillery truck

---
+++ games/CT/units/pnrocketartillery.lua
@@ -0,0 +1,201 @@
+	unitDef = {
+	  unitname            = [[pnrocketartillery]],
+	  name                = [[Rocky]],
+	  description         = [[ Adrian!!!!!!!]],
+	  acceleration        = 0.02,
+	  brakeRate           = 0.03,
+	  buildCostEnergy     = 700,
+	  buildCostMetal      = 700,
+	  builder             = false,
+	  buildPic            = [[pnrocketartillery.png]],
+	  buildTime           = 30,
+	  canAttack           = true,
+	  canGuard            = true,
+	  canMove             = true,
+	  canPatrol           = true,
+	  canStop             = true,
+	  category            = [[LAND]],
+	  corpse              = [[DEAD]],
+          reclaimable         = false,
+	
+	  explodeAs           = [[BIG_UNIT]],
+	  footprintX          = 5,
+	  footprintZ          = 5,
+	  highTrajectory      = 2,
+	  idleAutoHeal        = 0,
+	  leaveTracks         = true,
+	  mass                = 950,
+	  maxDamage           = 3000,
+	  maxSlope            = 18,
+	  maxVelocity         = 1,
+	  maxWaterDepth       = 22,
+	  minCloakDistance    = 75,
+	  movementClass       = [[TANK5x5]],
+	  noAutoFire          = false,
+	  noChaseCategory     = [[MINERALS FIXEDWING GUNSHIP SUB]],
+	  objectName          = [[pnrocketartillery.s3o]],
+	  seismicSignature    = 4,
+	  selfDestructAs      = [[BIG_UNIT]],
+
+
+	  sfxtypes            = {
+	
+	    explosiongenerators = {
+		"custom:orc_machinegun_flash",
+		"custom:orc_machinegun_muzzle",
+		"custom:orc_machinegun_flash_big",
+		"custom:orc_machinegun_muzzle_big",
+	    },
+	
+	  },
+
+
+
+	  sounds			= {
+	      select = {
+		"argh/Argh_TankSound",
+		},
+	      ok = {
+		"argh/Argh_TankSound",
+		},
+	  },
+
+	  side                = [[NKG]],
+	  sightDistance       = 750,
+	  smoothAnim          = true,
+	  trackOffset         = 8,
+	  trackStrength       = 10,
+	  trackStretch        = 1,
+	  trackType           = [[StdTank]],
+	  trackWidth          = 45,
+	  turnInPlace         = 1,
+	  turnRate            = 500,
+	  unitRestricted      = 5,
+          --script              = [[pnrocketartillery.lua]],
+	
+	  weapons             = {
+	
+	    {
+	      def                = [[Artillery]],
+	      badTargetCategory  = [[SWIM LAND SHIP HOVER]],
+	      mainDir            = [[0 0 1]],
+	      maxAngleDif        = 45,
+	      onlyTargetCategory = [[SWIM LAND SINK FLOAT SHIP HOVER]],
+	    },
+
+	    {
+	      def                = [[Cannon]],
+	      badTargetCategory  = [[SINK FLOAT]],
+	      onlyTargetCategory = [[LAND SINK SHIP SWIM FLOAT HOVER]],
+	    },
+	
+	  },
+	
+	
+	  weaponDefs          = {
+	
+	    Artillery             = {
+	      name                    = [[Heavy Artillery Cannon]],
+	      areaOfEffect            = 32,
+	      crater                  = 0.25,
+	
+	      damage                  = {
+		Cruiser = 1000,
+		Building = 2500,
+		Mech = 1000,
+		Tank = 1000,
+		Aircraft = 1000,
+		Ship = 1000,
+		Sub = 50,
+	      },
+	
+	      model                   = [[bshell.s3o]],
+	      explosionGenerator      = [[custom:LRPC_Expl]],
+	      interceptedByShieldType = 1,
+	      myGravity               = 0.1,
+	      range                   = 1000,
+	      reloadtime              = 7.5,
+	      soundHit                = [[argh/Argh_MedExplosion]],
+	      soundStart              = [[argh/Argh_Whamsound]],
+	      turret                  = true,
+	      weaponType              = [[Cannon]],
+	      weaponVelocity          = 750,
+	    },
+
+	    Cannon = {
+	      name                    = [[Light Cannon]],
+	      areaOfEffect            = 32,
+	      craterMult              = 0.25,
+	
+	      damage                  = {
+		Cruiser = 100,
+		Building = 100,
+		Mech = 100,
+		Tank = 100,
+		Aircraft = 100,
+		Ship = 100,
+		Sub = 5,
+	      },
+	
+	      model                   = [[bshell.s3o]],
+	      explosionGenerator      = [[custom:PLASMA_Expl]],
+	      interceptedByShieldType = 1,
+	      range                   = 340,
+	      reloadtime              = 3,
+	      soundHit                = [[argh/Argh_MedExplosion]],
+	      soundStart              = [[argh/Argh_heavy_machinegun]],
+	      turret                  = true,
+	      weaponType              = [[Cannon]],
+	      weaponVelocity          = 750,
+	    },
+	
+	  },
+	
+	
+	  featureDefs         = {
+	
+	    DEAD  = {
+	      description      = [[Wreckage - Very Heavy Siege Artillery]],
+	      blocking         = false,
+	      category         = [[corpses]],
+	      damage           = 2250,
+	      energy           = 0,
+	      featureDead      = [[DEAD2]],
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 2,
+	      footprintZ       = 2,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 760,
+	      object           = [[bgenericwreckage.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	
+	    DEAD2 = {
+	      description      = [[Debris - Very Heavy Siege Artillery]],
+	      blocking         = false,
+	      category         = [[heaps]],
+	      damage           = 1500,
+	      energy           = 0,
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 5,
+	      footprintZ       = 5,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 380,
+	      object           = [[b5x5heap.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	  },
+	
+	}
+	
+	return lowerkeys({ pnrocketartillery = unitDef })
