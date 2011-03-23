b3ae04853fd232e160c2b9a5fa1b2d67365a80e9
oksnoop2@gmail.com <oksnoop2@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-03-23 01:45:16 +0000

    adding pntanks

---
+++ games/CT/units/pnantimechtank.lua
@@ -0,0 +1,174 @@
+	unitDef = {
+	  unitname            = [[pnantimechtank]],
+	  name                = [[AntiMecc]],
+	  description         = [[anti mech missiles goooooo]],
+	  acceleration        = 0.4,
+	  brakeRate           = 0.2,
+	  buildCostEnergy     = 300,
+	  buildCostMetal      = 300,
+	  builder             = false,
+	  buildPic            = [[pnantimechtank.png]],
+	  buildTime           = 12,
+	  canAttack           = true,
+	  canGuard            = true,
+	  canMove             = true,
+	  canPatrol           = true,
+	  canStop             = true,
+	  category            = [[LAND]],
+	  corpse              = [[DEAD]],
+          reclaimable         = false,
+	
+	  explodeAs           = [[MEDIUM_UNIT]],
+	  footprintX          = 2,
+	  footprintZ          = 2,
+	  idleAutoHeal        = 0,
+	  leaveTracks         = true,
+	  mass                = 110,
+	  maxDamage           = 1000,
+	  maxSlope            = 18,
+	  maxVelocity         = 3,
+	  maxWaterDepth       = 22,
+	  minCloakDistance    = 75,
+	  movementClass       = [[TANK2x2]],
+	  moveState           = 0,
+	  noAutoFire          = false,
+	  noChaseCategory     = [[MINERALS LAND SINK HOVER SHIP FLOAT SUB]],
+	  objectName          = [[pnantimechtank.s3o]],
+	  seismicSignature    = 4,
+	  selfDestructAs      = [[MEDIUM_UNIT]],
+
+	  sounds			= {
+	      select = {
+		"argh/Argh_TankSound",
+		},
+	      ok = {
+		"argh/Argh_TankSound",
+		},
+	  },
+
+	  sfxtypes            = {
+	
+	    explosiongenerators = {
+		"custom:orc_machinegun_flash",
+		"custom:orc_machinegun_muzzle",
+	    },
+	
+	  },
+
+	  side                = [[NKG]],
+	  sightDistance       = 440,
+	  smoothAnim          = true,
+	  steeringmode        = [[1]],
+	  trackOffset         = 3,
+	  trackStrength       = 6,
+	  trackStretch        = 1,
+	  trackType           = [[StdTank]],
+	  trackWidth          = 24,
+	  turnInPlace         = 0,
+	  turnRate            = 500,
+	  workerTime          = 0,
+          script              = [[pnantimechtank.lua]],
+	
+	  weapons             = {
+	
+	    {
+	      def                = [[AA]],
+	      onlyTargetCategory = [[FIXEDWING GUNSHIP]],
+	    },
+	
+	  },
+	
+	
+	  weaponDefs          = {
+	
+	    AA         = {
+	      name                    = [[Homing AA Missile]],
+	      areaOfEffect            = 48,
+	      avoidFriendly           = true,
+	      canattackground         = false,
+	      cylinderTargetting      = 1,
+	      craterMult              = 0.25,
+	
+	      damage                  = {
+		Cruiser = 150,
+		Building = 20,
+		Mech = 20,
+		Tank = 20,
+		Aircraft = 150,
+		Ship = 20,
+		Sub = 10,
+	      },
+	
+	      explosionGenerator      = [[custom:PLASMA_Expl]],
+	      flightTime              = 3,
+	      interceptedByShieldType = 1,
+	      lineOfSight             = true,
+	      model                   = [[baamissile.s3o]],
+	      range                   = 530,
+	      reloadtime              = 2,
+	      smokedelay              = [[0.1]],
+	      smokeTrail              = true,
+	      soundHit                = [[argh/Argh_LargeExplosion]],
+	      soundStart              = [[evolutionRTS/aircraft_missile_01]],
+	      startVelocity           = 200,
+	      tolerance               = 22000,
+	      tracks                  = true,
+	      turret                  = true,
+	      turnRate                = 30000,
+	      weaponAcceleration      = 550,
+	      weaponTimer             = 5,
+	      weaponType              = [[MissileLauncher]],
+	      weaponVelocity          = 900,
+	    },
+	
+	  },
+	
+	
+	
+	  featureDefs         = {
+	
+	    DEAD  = {
+	      description      = [[Wreckage - Anti-Air Buggy]],
+	      blocking         = false,
+	      category         = [[corpses]],
+	      damage           = 825,
+	      energy           = 0,
+	      featureDead      = [[DEAD2]],
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 2,
+	      footprintZ       = 2,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 88,
+	      object           = [[bgenericwreckage.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	
+	    DEAD2 = {
+	      description      = [[Debris - Anti-Air Buggy]],
+	      blocking         = false,
+	      category         = [[heaps]],
+	      damage           = 550,
+	      energy           = 0,
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 3,
+	      footprintZ       = 3,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 44,
+	      object           = [[b3x3heap.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	  },
+	
+	}
+	
+	return lowerkeys({ pnantimechtank = unitDef })
---
+++ games/CT/units/pnradarvan.lua
@@ -0,0 +1,213 @@
+	unitDef = {
+	  unitname            = [[pnradarvan]],
+	  name                = [[radar]],
+	  description         = [[I've got more candy in teh back.]],
+	  acceleration        = 0.02,
+	  brakeRate           = 0.03,
+	  buildCostEnergy     = 700,
+	  buildCostMetal      = 700,
+	  builder             = false,
+	  buildPic            = [[pnradarvan.png]],
+	  buildTime           = 30,
+	  canAttack           = true,
+	  canGuard            = true,
+	  canMove             = true,
+	  canPatrol           = true,
+	  canStop             = true,
+	  category            = [[LAND]],
+	  corpse              = [[DEAD]],
+          reclaimable         = false,
+	
+	  explodeAs           = [[BIG_UNIT]],
+	  footprintX          = 5,
+	  footprintZ          = 5,
+	  highTrajectory      = 2,
+	  idleAutoHeal        = 0,
+	  leaveTracks         = true,
+	  mass                = 950,
+	  maxDamage           = 3000,
+	  maxSlope            = 18,
+	  maxVelocity         = 1,
+	  maxWaterDepth       = 22,
+	  minCloakDistance    = 75,
+	  movementClass       = [[TANK5x5]],
+	  noAutoFire          = false,
+	  noChaseCategory     = [[MINERALS FIXEDWING GUNSHIP SUB]],
+	  objectName          = [[pnradarvan.s3o]],
+	  seismicSignature    = 4,
+	  selfDestructAs      = [[BIG_UNIT]],
+
+
+	  sfxtypes            = {
+	
+	    explosiongenerators = {
+		"custom:orc_machinegun_flash",
+		"custom:orc_machinegun_muzzle",
+		"custom:orc_machinegun_flash_big",
+		"custom:orc_machinegun_muzzle_big",
+	    },
+	
+	  },
+
+
+
+	  sounds			= {
+	      select = {
+		"argh/Argh_TankSound",
+		},
+	      ok = {
+		"argh/Argh_TankSound",
+		},
+	  },
+
+	  side                = [[NKG]],
+	  sightDistance       = 750,
+	  smoothAnim          = true,
+	  trackOffset         = 8,
+	  trackStrength       = 10,
+	  trackStretch        = 1,
+	  trackType           = [[StdTank]],
+	  trackWidth          = 45,
+	  turnInPlace         = 1,
+	  turnRate            = 500,
+	  unitRestricted      = 5,
+          script              = [[pnradarvan.lua]],
+	
+	  weapons             = {
+        
+            {
+              def                = [[Rock_Drill]],
+              onlyTargetCategory = [[MINERALS]],
+            },
+
+	    {
+	      def                = [[Cannon]],
+	      badTargetCategory  = [[SINK FLOAT]],
+	      onlyTargetCategory = [[LAND SINK SHIP SWIM FLOAT HOVER]],
+	    },
+        
+          },
+	
+	
+	            weaponDefs          = {
+        
+            Rock_Drill = {
+              name                    = [[Rock Drill]],
+              areaOfEffect            = 8,
+              avoidFriendly           = 0,
+	      beamTime                = 0.2,
+              craterMult              = 0.25,
+              accuracy                = 0,
+	      collideFriendly         = false,
+	      energypershot           = 0,
+        
+              damage                  = {
+                Cruiser = 1,
+                Building = 1,
+                Mech = 1,
+                Tank = 1,
+                Aircraft = 1,
+                Ship = 1,
+                Sub = 1,
+              },
+        
+              explosionGenerator      = [[custom:resmining]],
+              interceptedByShieldType = 1,
+              lineOfSight             = true,
+              range                   = 130,
+              reloadtime              = 0.2,
+              rgbColor                = [[0 1 0]],
+              separation              = 2,
+	      thickness               = 1,
+	      minIntensity            = 1,
+              size                    = 0.4,
+              soundStart              = [[tp/swoosh]],
+              soundStartVolume        = 0.5,
+              soundTrigger            = true,
+              stages                  = 50,
+              targetBorder            = 1,
+              tolerance               = 8000,
+              turret                  = true,
+              weaponType              = [[BeamLaser]],
+              weaponVelocity          = 2500,
+
+
+		},
+
+	    Cannon = {
+	      name                    = [[Light Cannon]],
+	      areaOfEffect            = 32,
+	      craterMult              = 0.25,
+	
+	      damage                  = {
+		Cruiser = 100,
+		Building = 100,
+		Mech = 100,
+		Tank = 100,
+		Aircraft = 100,
+		Ship = 100,
+		Sub = 5,
+	      },
+	
+	      model                   = [[bshell.s3o]],
+	      explosionGenerator      = [[custom:PLASMA_Expl]],
+	      interceptedByShieldType = 1,
+	      range                   = 400,
+	      reloadtime              = 3,
+	      soundHit                = [[argh/Argh_MedExplosion]],
+	      soundStart              = [[argh/Argh_heavy_machinegun]],
+	      turret                  = true,
+	      weaponType              = [[Cannon]],
+	      weaponVelocity          = 750,
+	    },
+        
+          },
+	
+	
+	  featureDefs         = {
+	
+	    DEAD  = {
+	      description      = [[Wreckage - Very Heavy Siege Artillery]],
+	      blocking         = false,
+	      category         = [[corpses]],
+	      damage           = 2250,
+	      energy           = 0,
+	      featureDead      = [[DEAD2]],
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 2,
+	      footprintZ       = 2,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 760,
+	      object           = [[bgenericwreckage.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	
+	    DEAD2 = {
+	      description      = [[Debris - Very Heavy Siege Artillery]],
+	      blocking         = false,
+	      category         = [[heaps]],
+	      damage           = 1500,
+	      energy           = 0,
+	      featurereclamate = [[SMUDGE01]],
+	      footprintX       = 5,
+	      footprintZ       = 5,
+	      height           = [[4]],
+	      hitdensity       = [[100]],
+	      metal            = 380,
+	      object           = [[b5x5heap.s3o]],
+	      reclaimable      = true,
+	      reclaimTime      = 15,
+	      seqnamereclamate = [[TREE1RECLAMATE]],
+	      world            = [[All Worlds]],
+	    },
+	
+	  },
+	
+	}
+	
+	return lowerkeys({ pnradarvan = unitDef })
