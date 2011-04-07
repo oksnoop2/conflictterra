81e5d2bf738e36483425e79531c82a25977adc4c
sanadaujiosan@gmail.com <sanadaujiosan@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-04-07 00:51:46 +0000

    - Mech Cruiser Factory now has wreckage!

--- games/CT/units/bmechcruiserfactory.lua
+++ games/CT/units/bmechcruiserfactory.lua
@@ -81,47 +81,34 @@ unitDef = {
   featureDefs                   = {
 
     DEAD  = {
-      description      = [[Wreckage - Combat Mech Factory]],
+      description      = [[Wreckage - Saito Class Cruiser Mech Factory]],
       blocking         = true,
       category         = [[corpses]],
-      damage           = 6375,
+      damage           = 2250,
       energy           = 0,
       featureDead      = [[DEAD2]],
-      featurereclamate = [[SMUDGE01]],
-      footprintX       = 7,
-      footprintZ       = 6,
-      height           = [[40]],
-      hitdensity       = [[100]],
-      metal            = 200,
-      object           = [[bgenericwreckage.s3o]],
+      footprintX       = 16,
+      footprintZ       = 17,
+      metal            = 300,
+      object           = [[wrecks/bmechcruiserfactorywreck.s3o]],
       reclaimable      = true,
       reclaimTime      = 6375,
-
-      seqnamereclamate = [[TREE1RECLAMATE]],
-      world            = [[All Worlds]],
     },
 
 
     DEAD2 = {
-      description      = [[Debris - Combat Mech Factory]],
+      description      = [[Debris - Saito Class Cruiser Mech Factory]],
       blocking         = false,
 
       category         = [[heaps]],
-      damage           = 4250,
+      damage           = 1125,
       energy           = 0,
-      featurereclamate = [[SMUDGE01]],
-      footprintX       = 6,
-      footprintZ       = 6,
-      height           = [[4]],
-      hitdensity       = [[100]],
-      metal            = 100,
-      object           = [[b6x6heap.s3o]],
+      footprintX       = 16,
+      footprintZ       = 16,
+      metal            = 150,
+      object           = [[b16x16heap.s3o]],
       reclaimable      = true,
-      reclaimTime      = 275,
-      seqnamereclamate = [[TREE1RECLAMATE]],
-      world            = [[All Worlds]],
-
-    },
+      reclaimTime      = 275,    },
 
 
   },
