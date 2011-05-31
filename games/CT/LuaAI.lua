d6e9bc2dd9545591039c22945e2f6d03e081a836
anmeldekram71@web.de <anmeldekram71@web.de@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-05-31 15:43:16 +0000

    waitwut

--- games/CT/LuaAI.lua
+++ games/CT/LuaAI.lua
@@ -3,5 +3,9 @@ local listOfLuaAIs = {
         name = "KTAI",
         desc = "knorkes TEST ai. CRAS HDANGER. safer to watch on a video: http://www.youtube.com/watch?v=aOtnOEOtVTM",
     },
+    {
+        name = "SchwarmAI",
+        desc = "drrrrrooooooones",
+    },
 }
 return listOfLuaAIs
