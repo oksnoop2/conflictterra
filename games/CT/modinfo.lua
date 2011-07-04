eedc8c58b0d680f5780d55c3edbeddfd494f32a9
anmeldekram71@web.de <anmeldekram71@web.de@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-07-04 13:34:13 +0000

    reverted to version = "$VERSION",
    the $version is used as a placeholder, so that after every commit a new downloadable game version is automatically available.
    
    to avoid conflicts with your "play online" and "develop offline" game versions, just change your offline version to 
    version = "my offline test version",
    or something similiar.
    And then only commit the files you actually want to commit instead of all differing files.

--- games/CT/modinfo.lua
+++ games/CT/modinfo.lua
@@ -1,17 +1,17 @@
 local modinfo = {
-	name = "Conflict Terra DEV COPY",
-	shortname = "ct DEV COPY",
-	version = "DEV COPY",
-	game = "Conflict Terra DEV COPY",
-	shortgame = "ctdevcopy",
-	description = "Conflict Terra DEV COPY",
-	url = "http://www.moddb.com/games/conflict-terra",
-	
-	modtype = "1",
+        name = "Conflict Terra",
+        shortname = "ct",
+        version = "$VERSION",
+        game = "Conflict Terra",
+        shortgame = "ct",
+        description = "Conflict Terra",
+        url = "http://www.moddb.com/games/conflict-terra",
+        
+        modtype = "1",
 
-	depend = {
-		"cursors.sdz",
-	}
+        depend = {
+                "cursors.sdz",
+        }
 }
  
 return modinfo 
