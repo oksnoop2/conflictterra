c5c704a2e8cb30631f8dff676d1a44c4831ea7fb
rothj1459@gmail.com <rothj1459@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2011-07-26 14:56:44 +0000

    ==split YMSAI into YMSAI_stable and YMSAI_test branches. The stable branch works (not very good at managing it's enconomy, but it works), and the test branch may or may not work on any given revision as I continue to tinker.

--- games/CT/LuaAI.lua
+++ games/CT/LuaAI.lua
@@ -1,9 +1,9 @@
 local listOfLuaAIs = {
     
-    {
-        name = "NullLuaAI",
-        desc = "This AI does absolutely nothing",
-    },
+--    {
+--        name = "NullLuaAI",
+--        desc = "This AI does absolutely nothing",
+--    },
     {
         name = "KTAI",
         desc = "knorkes TEST ai. CRASH DANGER. safer to watch on a video: http://www.youtube.com/watch?v=aOtnOEOtVTM",
@@ -13,9 +13,14 @@ local listOfLuaAIs = {
         desc = "Knorke's AI for the drone's faction",
     },
 	{
-        name = "YMSAI",
-        desc = "Yanom's Modified Schwarm AI",
+        name = "YMSAI_test",
+        desc = "Yanom's Modified Schwarm AI -experimental- may not work",
     },
+    {
+        name = "YMSAI_stable",
+        desc = "Yanom's Modified Schwarm AI -stable branch- PLAYS DRONE FACTION ONLY. Drone faction icon looks like two squares.",
+    },
+    
     
 }
 
