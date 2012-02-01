78a13c8053d49bfafe9d0ddee329f547d99c79be
sanadaujiosan@gmail.com <sanadaujiosan@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2012-02-01 00:06:17 +0000

    - Retextured the bfighter.  Gave it landing gear which retract and hide while in flight.  No one will probably ever see it, but I like it being there :)

--- games/CT/scripts/bfightermkii.lua
+++ games/CT/scripts/bfightermkii.lua
@@ -1,20 +1,63 @@
-	-- by KR
+	--Hayabusa Type Fighter MkII animation script by Sanada
 	
 	--pieces
 	local fuselage = piece "fuselage"
+
+	local frontgear = piece "frontgear"
+	local leftgear = piece "leftgear"
+	local rightgear = piece "rightgear"
+
 	local flare1 = piece "flare1"
 	local flare2 = piece "flare2"
 	local fxflare1 = piece "fxflare1"
 	local fxflare2 = piece "fxflare2"
-	local fxflare3 = piece "fxflare3"
-	local fxflare4 = piece "fxflare4"
 
+
+	--variables
 	local currBarrel = 1
+
 
 	--signals
 	local SIG_AIM = 1
 	
+
+	--local functions
+	local function landinggear()
+		--landing gear retract
+		Turn(frontgear, x_axis, -1.5, 1)
+		Turn(leftgear, x_axis, -1.5, 1)
+		Turn(rightgear, x_axis, -1.5, 1)
+
+		--landing gear wait
+		WaitForTurn(frontgear, x_axis)
+		WaitForTurn(leftgear, x_axis)
+		WaitForTurn(rightgear, x_axis)
+
+		--landing gear hide
+		Hide(frontgear)
+		Hide(leftgear)
+		Hide(rightgear)
+	end
+
+
+	--script
 	function script.Create()
+	end
+
+	function script.Activate()
+		StartThread(landinggear)
+	end
+
+	function script.Deactivate()
+		--landing gear show
+		Show(frontgear)
+		Show(leftgear)
+		Show(rightgear)
+
+		--landing gear extend
+		Turn(frontgear, x_axis, 0, 1)
+		Turn(leftgear, x_axis, 0, 1)
+		Turn(rightgear, x_axis, 0, 1)
 	end
 
 	function script.QueryWeapon1()
