1936408ee8847d9dbb45713a6dd5ff1fe485a409
sanadaujiosan@gmail.com <sanadaujiosan@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2012-03-01 08:58:23 +0000

    - Removed transport cruiser's weapons.

--- games/CT/scripts/btransportcruiser.lua
+++ games/CT/scripts/btransportcruiser.lua
@@ -1,4 +1,7 @@
+	--Takeda Class Cruiser animation script by Sanada
+	--Lots of transport help from prominent members of the Spring community
 
+	--pieces
 	local body = piece "body"
 
 	local forljet = piece "forljet"
@@ -17,105 +20,18 @@
 	local flare4 = piece "flare3"
 	local flare3 = piece "flare4"
 
-	local currBarrel = 1
-	local currBarrel2 = 1
-
+	
+	--locals
 	local AttachUnit = Spring.UnitScript.AttachUnit
 
+	
+	--signals
 	local SIG_AIM = 1
 	local SIG_AIM_SEC = 2
-        local ct_missile_smokecloud = SFX.CEG
 
 
+	--script
 	function script.Create(unitID)
-	end
-
-	local function RestoreAfterDelay(unitID)
-		Sleep(2500)
-		Turn(flare1, y_axis, 0, math.rad(90))
-		Turn(flare2, y_axis, 0, math.rad(90))
-        	Turn(flare1, x_axis, 0, math.rad(50))
-        	Turn(flare2, x_axis, 0, math.rad(50))
-	end
-
-	local function RestoreAfterDelay2(unitID)
-		Sleep(2500)
-		Turn(flare3, y_axis, 0, math.rad(90))
-		Turn(flare4, y_axis, 0, math.rad(90))
-        	Turn(flare3, x_axis, 0, math.rad(50))
-        	Turn(flare4, x_axis, 0, math.rad(50))
-	end
-
-	function script.QueryWeapon1()
-		if (currBarrel == 1) then 
-			return flare1
-		else 
-			return flare2
-		end
-	end
-
-	function script.QueryWeapon2()
-		if (currBarrel2 == 1) then 
-			return flare3
-		else 
-			return flare4
-		end
-	end
-	
-	function script.AimFromWeapon1() return body end
-
-        function script.AimFromWeapon2() return body end
-	
-	function script.AimWeapon1( heading, pitch )
-                Signal(SIG_AIM)
-		SetSignalMask(SIG_AIM)
-        	Turn(flare1, y_axis, heading, math.rad(90))
-        	Turn(flare2, y_axis, heading, math.rad(90))
-        	Turn(flare1, x_axis, -pitch, math.rad(50))
-        	Turn(flare2, x_axis, -pitch, math.rad(50))
-        	WaitForTurn(flare1, y_axis)
-        	WaitForTurn(flare2, y_axis)
-        	WaitForTurn(flare1, x_axis)
-        	WaitForTurn(flare2, x_axis)
-		StartThread(RestoreAfterDelay)
-		return true
-	end
-
-	function script.AimWeapon2( heading, pitch )
-		Signal(SIG_AIM_SEC)
-		SetSignalMask(SIG_AIM_SEC)
-        	Turn(flare3, y_axis, heading, math.rad(90))
-        	Turn(flare4, y_axis, heading, math.rad(90))
-        	Turn(flare3, x_axis, -pitch, math.rad(50))
-        	Turn(flare4, x_axis, -pitch, math.rad(50))
-        	WaitForTurn(flare3, y_axis)
-        	WaitForTurn(flare4, y_axis)
-        	WaitForTurn(flare3, x_axis)
-        	WaitForTurn(flare4, x_axis)
-		StartThread(RestoreAfterDelay2)
-		return true
-	end
-	
-	function script.FireWeapon1()
-		if currBarrel == 1 then
-			EmitSfx(flare2, ct_missile_smokecloud)
-		end
-		if currBarrel == 2 then
-			EmitSfx(flare1, ct_missile_smokecloud)
-		end
-		currBarrel = currBarrel + 1
-		if currBarrel == 3 then currBarrel = 1 end
-	end
-
-	function script.FireWeapon2()
-		if currBarrel2 == 1 then
-			EmitSfx(flare4, ct_missile_smokecloud)
-		end
-		if currBarrel2 == 2 then
-			EmitSfx(flare3, ct_missile_smokecloud)
-		end	
-		currBarrel2 = currBarrel2 + 1
-		if currBarrel2 == 3 then currBarrel2 = 1 end
 	end
 
 	function script.BeginTransport(passengerID)
--- games/CT/units/btransportcruiser.lua
+++ games/CT/units/btransportcruiser.lua
@@ -14,6 +14,7 @@
 	  canAttack           = true,
 	  canFly              = true,
 	  canGuard            = true,
+	  canFight            = false,
 	  canMove             = true,
 	  canPatrol           = true,
 	  canStop             = true,
@@ -41,7 +42,6 @@
 	  maxVelocity         = 8,
 	  minCloakDistance    = 75,
 	  noAutoFire          = false,
-	  noChaseCategory     = [[MINERALS SUB]],
 	  objectName          = [[btransportcarrier2.s3o]],
 	  releaseHeld         = true,
 	  seismicSignature    = 0,
@@ -60,14 +60,6 @@
 		},
 	  },
 
-	  sfxtypes            = {
-	
-	    explosiongenerators = {
-		"custom:ct_missile_smokecloud",
-	    },
-	
-	  },
-
 	  transportCapacity   = 40,
 	  transportMass       = 6050,
 	  transportSize       = 1000,
@@ -76,128 +68,6 @@
 	  transportUnloadMethod = 2,
 	  upright             = true,
 	  script              = [[btransportcruiser.lua]],
-
-	
-	  weapons             = {
-
-	    {
-	      def                = [[Ground]],
-	      badTargetCategory  = [[SINK FLOAT]],
-	      mainDir            = [[0 -1 1]],
-	      maxAngleDif        = 180,
-	      onlyTargetCategory = [[LAND SINK SHIP SWIM FLOAT HOVER]],
-	    },
-
-	    {
-	      def                = [[AA]],
-	      badTargetCategory  = [[GUNSHIP CRUISER]],
-	      mainDir            = [[0 0 1]],
-	      maxAngleDif        = 90,
-	      onlyTargetCategory = [[FIXEDWING GUNSHIP CRUISER]],
-	    },
-	
-	  },
-	
-	
-	  weaponDefs          = {
-	
-	    AA         = {
-	      name                    = [[Homing AA Missile]],
-	      areaOfEffect            = 48,
-	      avoidFriendly           = true,
-	      canattackground         = false,
-	      cylinderTargetting      = 1,
-	      cegtag                  = [[ct_missiletail_aa]],
-	      craterMult              = 0.25,
-	
-	      damage                  = {
-		default = 20,
-		Cruiser = 200,
-		Building = 20,
-		Mech = 20,
-		Tank = 20,
-		Aircraft = 200,
-		Ship = 20,
-		Sub = 20,
-
-		Meteor = 0,
-		Drone = 20,
-		Spare1 = 20,
-		Spare2 = 20,
-		Spare3 = 20,
-	      },
-	
-	      --explosionGenerator      = [[custom:PLASMA_Expl]],
-	      flightTime              = 3,
-	      interceptedByShieldType = 1,
-	      impulseFactor           = 0,
-	      lineOfSight             = true,
-	      model                   = [[projectiles/baamissile.s3o]],
-	      range                   = 600,
-	      reloadtime              = 3,
-	      smokeTrail              = true,
-	      soundHit                = [[argh/Argh_LargeExplosion]],
-	      soundStart              = [[]],
-	      texture2		      = [[ct_smoketrail]],
-	      startVelocity           = 200,
-	      tolerance               = 22000,
-	      tracks                  = true,
-	      turnRate                = 30000,
-	      turret                  = true,
-	      weaponAcceleration      = 550,
-	      weaponTimer             = 5,
-	      weaponType              = [[MissileLauncher]],
-	      weaponVelocity          = 750,
-	    },
-
-	    Ground = {
-	      name                    = [[Air-to-Ground Missile]],
-	      areaOfEffect            = 160,
-	      craterMult              = 0.25,
-	      cegTag                  = [[ct_missiletail_big]],
-	
-	      damage                  = {
-		default = 250,
-		Cruiser = 250,
-		Building = 250,
-		Mech = 250,
-		Tank = 250,
-		Aircraft = 250,
-		Ship = 250,
-		Sub = 250,
-
-		Meteor = 0,
-		Drone = 250,
-		Spare1 = 250,
-		Spare2 = 250,
-		Spare3 = 250,
-	      },
-	
-	      model                   = [[projectiles/bsmallmissile.s3o]],
-	      edgeEffectiveness       = 0.5,
-	      --explosionGenerator      = [[custom:PLASMA_Expl]],
-	      flightTime              = 3,
-	      interceptedByShieldType = 1,
-	      impulseFactor           = 0,
-	      lineOfSight             = true,
-	      range                   = 600,
-	      reloadtime              = 3,
-	      smokeTrail              = true,
-	      soundHit                = [[argh/Argh_LargeExplosion]],
-	      soundStart              = [[nexuiz/rocket_fire]],
-	      texture2		      = [[ct_smoketrail]],
-	      tolerance               = 4000,
-	      trajectoryHeight        = 0.5,
-	      tracks                  = true,
-	      turnRate                = 1000,
-	      turret                  = true,
-	      weaponType              = [[MissileLauncher]],
-	      weaponVelocity          = 400,
-	      startVelocity           = 400,
-	      weaponAcceleration      = 70,
-	    },
-
-          },
 	
 	
 	  featureDefs         = {
