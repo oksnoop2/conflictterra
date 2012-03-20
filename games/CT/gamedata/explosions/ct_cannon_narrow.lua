564035382391dbc1c81a66dfcafb28b7b03e0386
sanadaujiosan@gmail.com <sanadaujiosan@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2012-03-20 08:06:06 +0000

    - Updated ct_cannon_narrow to have particles, and to be in the same style as ct_cannon_tank (only smaller) - Kept the old CEG as ct_cannon_narrow_old

--- games/CT/gamedata/explosions/ct_cannon_narrow.lua
+++ games/CT/gamedata/explosions/ct_cannon_narrow.lua
@@ -1,44 +1,72 @@
-	--ct_cannon_narrow
-	--Narrow muzzle flare for cannon
-	--by Sanada
+--ct_cannon_narrow
+--Narrow muzzle flare for cannon (particles)
+--by Sanada
 
-	return {
-		["ct_cannon_narrow"] = {
-			bitmapmuzzleflame1 = {
-				ground             = true,
-				air                = true,
-				water              = true,
-				class              = [[CBitmapMuzzleFlame]],
-				count              = 1,
-				underwater         = 1,
-				properties = {
-					colormap           = [[1 1 0 1	    1 1 0 1     1 0.5 0 1]], --Colour map progress from one to next. Colors in RGBA
-					dir                = [[dir]],
-					frontoffset        = 0,
-					fronttexture       = [[ct_cannon_tank]],
-					length             = 15,
-					sidetexture        = [[ct_cannon_artillery_side]],
-					size               = 3,
-					sizegrowth         = 1,
-					ttl                = 5,
-				},
-			},
+return {
+  ["ct_cannon_narrow"] = {
 
-			groundflash = {
-				air                = true,
-				ground             = true,
-				water              = true,
-				circlealpha        = 0.6,
-				circlegrowth       = 0,
-				flashalpha         = 0.2,
-				flashsize          = 20,
-				ttl                = 8, --Time To Live
-				--Red Green Blue
-				color = {
-					[1]  = 1,
-					[2]  = 0.8,
-					[3]  = 0,
-				},
-			},
-		},
-	}
+    ring = {
+      air                = true,
+      class              = [[CSimpleParticleSystem]],
+      count              = 1,
+      ground             = true,
+      underwater         = 1,
+      useairlos          = true,
+      water              = true,
+      alwaysVisible      = true,
+	  properties = {
+        alwaysVisible      = true,
+	    airdrag            = 0.95,
+        colormap           = [[1 1 0 0.25	    1 0.75 0 0.15     1 0.5 0 0.05]],
+        directional        = 1,
+        emitrot            = 90,
+        emitrotspread      = 0,
+        emitvector         = [[dir]],
+        gravity            = [[0, 0, 0]],
+        numparticles       = 20,
+        particlelife       = 3,
+        particlelifespread = 2,
+        particlesize       = 0.5,
+        particlesizespread = 1,
+        particlespeed      = 2,
+        particlespeedspread = 1,
+        pos                = [[0, 0, 0]],
+        sizegrowth         = 0.5,
+        sizemod            = 1,
+        texture            = [[ct_smoke]],
+      },
+    },  
+  
+    poof = {
+      air                = true,
+      class              = [[CSimpleParticleSystem]],
+      count              = 1,
+      ground             = true,
+      underwater         = 1,
+      useairlos          = true,
+      water              = true,
+      alwaysVisible      = true,
+	  properties = {
+        alwaysVisible      = true,
+	    airdrag            = 0.95,
+        colormap           = [[1 1 0 0.25	    1 0.75 0 0.15     1 0.5 0 0.05]],
+        directional        = 1,
+        emitrot            = 10,
+        emitrotspread      = 10,
+        emitvector         = [[dir]],
+        gravity            = [[0, 0, 0]],
+        numparticles       = 10,
+        particlelife       = 9,
+        particlelifespread = 4,
+        particlesize       = 0.75,
+        particlesizespread = 1,
+        particlespeed      = 2,
+        particlespeedspread = 1,
+        pos                = [[0, 0, 0]],
+        sizegrowth         = 0.5,
+        sizemod            = 1,
+        texture            = [[ct_smoke]],
+      },
+    },	
+  },
+}
