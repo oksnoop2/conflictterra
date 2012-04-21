2a65289b8154a66c4a29302e925e897f50387c21
rothj1459 <rothj1459@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2012-04-21 01:59:32 +0000

    made the drone engineer build/repair a bit slower. It was toooo fast, fast enough to make mid-combat repairs OP

--- games/CT/units/kdroneengineer.lua
+++ games/CT/units/kdroneengineer.lua
@@ -74,7 +74,7 @@ local kdroneengineer = {
 	turnRate            = 1000,
 	showNanospray	    = 1,
 	Resurrectspeed      = 100,
-	workerTime          = 1,
+	workerTime          = .7,
 	script              = "kdroneengineer.lua",
 
 	featureDefs         = {
