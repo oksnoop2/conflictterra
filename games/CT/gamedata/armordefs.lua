a5d4b4bd1eaa17c3ade720644ce571aeb399d2c0
rothj1459 <rothj1459@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2012-09-06 01:06:03 +0000

    edited kdroneminerflyer.lua again gutted armordefs.lua

--- games/CT/gamedata/armordefs.lua
+++ games/CT/gamedata/armordefs.lua
@@ -1,204 +1,206 @@
-	--Armor Defs
+    --Armor Defs
 
 
-	local armorDefs = {
+    local armorDefs = {
 
-		Cruiser  =  {
-			--NKG--
-
-
-			--combat cruisers
-			"bprimarycruiser",
-			"bflagship",
-			"bmissilecruiser",
-			"bbattlecruiser",
-			"btransportcruiser",
-
-			--economy cruisers
-			"bsupplydepotmobile",
-
-			--factory cruisers
-			"bmechcruiser",
-			"btankcruiser",
-			"baircruiser",
-  		},
+        --[[Cruiser  =  {
+            --NKG--
 
 
-		Building  =  {
-			--NKG--
+            --combat cruisers
+            "bprimarycruiser",
+            "bflagship",
+            "bmissilecruiser",
+            "bbattlecruiser",
+            "btransportcruiser",
+
+            --economy cruisers
+            "bsupplydepotmobile",
+
+            --factory cruisers
+            "bmechcruiser",
+            "btankcruiser",
+            "baircruiser",
+        },
 
 
-			--landed cruisers
-			--T1
-			"bprimarycruiserbase",
-			"bprimarycruiserbaseai",
-			"bsupplydepot",
-			"bsupplydepotai",
-			"bmechcruiserfactory",
-			"btankcruiserfactory",
-			"baircruiserfactory",
-
-			--economy buildings
-			"bmex",
-			"bpowerplant",
-			"badvpowerplant",
-			"bseapowerplant",
-
-			--defenses
-			--T1
-			"bantitankmine",
-			"bcameratower",
-			"bradartower",
-			"bjammertower",
-			"blandturret",
-			"bairturret",
-			"blandnavalturret",
-			"bgatlingturret",
-			"bsearadartower",
-			"btorpedoturret",
-			--T2
-			"badvlandturret",
-			"badvairturret",
-			"badvlandnavalturret",
-			"badvgatlingturret",
-			"badvsearadartower",
-			"badvtorpedoturret",
-
-			--factory
-			--T1
-			"bbeacon",
-			"bshipyard",
+        Building  =  {
+            --NKG--
 
 
-			--DRONE--
+            --landed cruisers
+            --T1
+            "bprimarycruiserbase",
+            "bprimarycruiserbaseai",
+            "bsupplydepot",
+            "bsupplydepotai",
+            "bmechcruiserfactory",
+            "btankcruiserfactory",
+            "baircruiserfactory",
+
+            --economy buildings
+            "bmex",
+            "bpowerplant",
+            "badvpowerplant",
+            "bseapowerplant",
+
+            --defenses
+            --T1
+            "bantitankmine",
+            "bcameratower",
+            "bradartower",
+            "bjammertower",
+            "blandturret",
+            "bairturret",
+            "blandnavalturret",
+            "bgatlingturret",
+            "bsearadartower",
+            "btorpedoturret",
+            --T2
+            "badvlandturret",
+            "badvairturret",
+            "badvlandnavalturret",
+            "badvgatlingturret",
+            "badvsearadartower",
+            "badvtorpedoturret",
+
+            --factory
+            --T1
+            "bbeacon",
+            "bshipyard",
 
 
-			"kgrounddronestructure",
-			"kdroneminingtower",
-			"kairdronestructure",	
-		},
-
-		Land  =  {
-			--NKG--
+            --DRONE--
 
 
-			--T1
-			--Mech Cruiser
-			"bengineer",
-			"bengineerai",
-			"bminer",
-			"bscoutmech",
-			"bbasicmech",
-			"bmissilemech",
-			"bantitankmech",
-			"bsnipermech",
-			"bacemech",
-			
-			--Tank Cruiser
-			"bscoutvehicle",
-			"bassaulttank",
-			"baabuggy",
-			"breztank",
-			"bartillery",
-			"bgeneraltank",			
-			--T2
-			--Mech Cruiser
-			"bassaultmech",
-			"bheavymissilemech",
-			"bheavyassaultmech",
-			
-			--Tank Cruiser
-			"bheavyassaulttank",
-			"bheavymissiletank",
-			"bvhsa",		
+            "kgrounddronestructure",
+            "kdroneminingtower",
+            "kairdronestructure",   
+        },
+
+        Land  =  {
+            --NKG--
 
 
-			--DRONE--
+            --T1
+            --Mech Cruiser
+            "bengineer",
+            "bengineerai",
+            "bminer",
+            "bscoutmech",
+            "bbasicmech",
+            "bmissilemech",
+            "bantitankmech",
+            "bsnipermech",
+            "bacemech",
+            
+            --Tank Cruiser
+            "bscoutvehicle",
+            "bassaulttank",
+            "baabuggy",
+            "breztank",
+            "bartillery",
+            "bgeneraltank",         
+            --T2
+            --Mech Cruiser
+            "bassaultmech",
+            "bheavymissilemech",
+            "bheavyassaultmech",
+            
+            --Tank Cruiser
+            "bheavyassaulttank",
+            "bheavymissiletank",
+            "bvhsa",        
 
 
-			"kdroneengineer",
-			"kdronewarrior",
-			"klightdrone",
-			"kdroneroller",
-			"ktridroneroller",
-		},
-
-		Aircraft = {
-			--NKG--
+            --DRONE--
 
 
-			--flying mechs
-			"bflyingmech",
-			"bheavyflyingmech",
+            "kdroneengineer",
+            "kdronewarrior",
+            "klightdrone",
+            "kdroneroller",
+            "ktridroneroller",
+        },
 
-			--aircraft
-			"bscoutplane",
-			"bfighter",
-			"blaserplane",
-			"brocketplane",
-			"bbomber",
-
-
-			--DRONE--
+        Aircraft = {
+            --NKG--
 
 
-			"kairdrone",
-			"kdiairdrone",
-			"ktriairdrone",
+            --flying mechs
+            "bflyingmech",
+            "bheavyflyingmech",
+
+            --aircraft
+            "bscoutplane",
+            "bfighter",
+            "blaserplane",
+            "brocketplane",
+            "bbomber",
 
 
-			--PNF--
-
-			--T1
-			"pnfighter",		
-		},
-
-		Ship = {
-			--NKG--
+            --DRONE--
 
 
-			"bseaengineer1",
-			"bseaengineer2",
-			"bbattleship",
-			"brocketbattleship",
-			"bartilleryship",
-			"baaship",
-			"bradarship",
-		},
-
-		Sub = {
-			--NKG--
+            "kairdrone",
+            "kdiairdrone",
+            "ktriairdrone",
 
 
-			"bsubmarine",
-		},
+            --PNF--
 
-		Meteor = {
-			"bmeteorimpact",
-			"bmeteorimpact_big",
-			"bminerals",
-		},
+            --T1
+            "pnfighter",        
+        },
 
-		Drone = {
-		},
+        Ship = {
+            --NKG--
 
-		Spare1 = {
-		},
 
-		Spare2 = {
-		},
+            "bseaengineer1",
+            "bseaengineer2",
+            "bbattleship",
+            "brocketbattleship",
+            "bartilleryship",
+            "baaship",
+            "bradarship",
+        },
 
-		Spare3 = {
-		},
+        Sub = {
+            --NKG--
 
-	}
 
-	for categoryName, categoryTable in pairs(armorDefs) do
-		local t = {}
-		for _, unitName in pairs(categoryTable) do
-			t[unitName] = 1
-		end
-		armorDefs[categoryName] = t
-	end
+            "bsubmarine",
+        }, 
 
-	return armorDefs
+
+        Drone = {
+        },
+
+        Spare1 = {
+        },
+
+        Spare2 = {
+        },
+
+        Spare3 = {
+        },]]--
+
+        Meteor = {
+            "bmeteorimpact",
+            "bmeteorimpact_big",
+            "bminerals",
+        },
+
+
+    }
+
+    for categoryName, categoryTable in pairs(armorDefs) do
+        local t = {}
+        for _, unitName in pairs(categoryTable) do
+            t[unitName] = 1
+        end
+        armorDefs[categoryName] = t
+    end
+
+    return armorDefs
