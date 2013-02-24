986479168f15c9820da041a9eff745e6044f16a1
sanadaujiosan@gmail.com <sanadaujiosan@gmail.com@2bc0353c-52ca-f4ea-b299-431aeee67e7d>
2013-02-24 05:35:27 +0000

    - Remodeled the Radar Outpost.  Larger, beefier, looks way cooler and has lights!  And kept its spinning dish :)  No stats were changed, just appearance.

--- games/CT/scripts/bradartower.lua
+++ games/CT/scripts/bradartower.lua
@@ -1,30 +1,44 @@
-local building = piece "building"
-local stem = piece "stem"
-local dish = piece "dish"
+	--Radar Outpost animation script by Sanada
+
+	--pieces
+	local body = piece "body"
+	local dish = piece "dish"
+	local fxflare1 = piece "fxflare1"
+	local fxflare2 = piece "fxflare2"
 
 
-local function dish_animation()
-	Move (stem, y_axis, 10, 50)
-	WaitForMove (stem, y_axis)
-	Turn (dish, z_axis, 1.55, 10)
-	WaitForTurn (dish, z_axis)
-	Spin (dish, y_axis, 1, 5)
-end
+	--signals
+	local SIG_LIGHTS_RED = 1
+	
+	
+	--CEGs
+	local ct_buildlight_red = SFX.CEG
+	
+	
+	--local functions
+	local function lights()
+		Signal(SIG_LIGHTS_RED)
+		SetSignalMask(SIG_LIGHTS_RED)
+		while (true) do
+			EmitSfx(fxflare1, ct_buildlight_red)
+			Sleep(2000)
+			EmitSfx(fxflare2, ct_buildlight_red)
+			Sleep(3000)
+		end
+	end
 
-local function dish_stop()
-	StopSpin (dish, y_axis, 5)
-	Turn (dish, z_axis, 0, 10)
-	WaitForTurn (dish, z_axis)
-end
+	
+	--script
+	function script.Activate()
+		StartThread(lights)
+		Spin (dish, y_axis, 1, 5)
+	end
 
-function script.Activate()
-	StartThread(dish_animation)
-end
+	function script.Deactivate()
+		Signal (SIG_LIGHTS_RED)
+		StopSpin (dish, y_axis, 5)
+	end
 
-function script.Deactivate()
-	StartThread(dish_stop)
-end
-
-function script.Killed(recentDamage, maxHealth)
-	return 0
-end
+	function script.Killed(recentDamage, maxHealth)
+		return 0
+	end
--- games/CT/units/bradartower.lua
+++ games/CT/units/bradartower.lua
@@ -1,97 +1,104 @@
-unitDef = {
+	--Radar Outpost Unit Def by Sanada
+	--Notes:  NOT Balanced Version 1
 
-  unitname                      = [[bradartower]],
-  name                          = [[Radar Outpost]],
-  description                   = [[Detects units, even cloaked enemies. Uses 10 energy per second to operate.]],
-  amphibious                    = false,
-  activateWhenBuilt             = true,
-  buildCostEnergy               = 100,
-  buildCostMetal                = 100,
-  builder                       = false,
+	unitDef = {
 
-  buildPic                      = [[bradartower.png]],
-  buildTime                     = 20,
-  canAttack                     = false,
-  category                      = [[SINK]],
-  corpse                        = [[DEAD]],
-  reclaimable                   = false,
+	  unitname                      = [[bradartower]],
+	  name                          = [[Radar Outpost]],
+	  description                   = [[Detects units, even cloaked enemies. Uses 10 energy per second to operate.]],
+	  amphibious                    = false,
+	  activateWhenBuilt             = true,
+	  buildCostEnergy               = 100,
+	  buildCostMetal                = 100,
+	  builder                       = false,
 
-  collisionVolumeOffsets        = [[0 0 0]],
-  collisionVolumeScales         = [[16 64 16]],
-  collisionVolumeTest           = 1,
-  collisionVolumeType           = [[box]],
+	  buildPic                      = [[bradartower.png]],
+	  buildTime                     = 20,
+	  canAttack                     = false,
+	  category                      = [[SINK]],
+	  corpse                        = [[DEAD]],
+	  reclaimable                   = false,
 
-  energyUse                     = 10,
-  explodeAs                     = [[SMALL_BUILDING]],
-  footprintX                    = 1,
-  footprintZ                    = 1,
-  idleAutoHeal                  = 0,
-  isTargetingUpgrade            = false,
-  levelGround                   = false,
-  mass                          = 27.5,
-  maxDamage                     = 500,
-  maxSlope                      = 36,
-  maxWaterDepth                 = 0,
-  noAutoFire                    = false,
-  objectName                    = [[bradartower.s3o]],
+	  collisionVolumeOffsets        = [[0 0 0]],
+	  collisionVolumeScales         = [[48 140 48]],
+	  collisionVolumeTest           = 1,
+	  collisionVolumeType           = [[box]],
 
-  onoffable                     = true,
-  radarDistance                 = 2000, --3000 (adv range...)
-  seismicSignature              = 4,
-  selfDestructAs                = [[SMALL_BUILDING]],
-  side                          = [[NKG]],
-  sightDistance                 = 400,
-  smoothAnim                    = true,
-  yardMap                       = [[o]],
-  script                        = [[bradartower.lua]],
+	  energyUse                     = 10,
+	  explodeAs                     = [[SMALL_BUILDING]],
+	  footprintX                    = 3,
+	  footprintZ                    = 3,
+	  idleAutoHeal                  = 0,
+	  isTargetingUpgrade            = false,
+	  levelGround                   = false,
+	  mass                          = 27.5,
+	  maxDamage                     = 500,
+	  maxSlope                      = 36,
+	  maxWaterDepth                 = 0,
+	  noAutoFire                    = false,
+	  objectName                    = [[bradartower.s3o]],
+  
+	  sounds			= {
+        select = {
+	    "ct/radar_chatter",
+	    },
 
-  sounds			= {
-      select = {
-	"ct/radar_chatter",
-	},
+        ok = {
+	    "ct/radar_chatter",
+	    },
+	  },
+  
+	  sfxtypes            = {
+	    explosiongenerators = {
+	    "custom:ct_buildlight_red",
+	    },
+	  },
 
-      ok = {
-	"ct/radar_chatter",
-	},
-  },
+	  onoffable                     = true,
+	  radarDistance                 = 2000, --3000 (adv range...)
+	  seismicSignature              = 4,
+	  selfDestructAs                = [[SMALL_BUILDING]],
+	  side                          = [[NKG]],
+	  sightDistance                 = 400,
+	  smoothAnim                    = true,
+	  yardMap                       = [[ooo ooo ooo]],
+	  script                        = [[bradartower.lua]],
+
+  
+	  featureDefs                   = {
+
+	    DEAD  = {
+        description      = [[Wreckage - Radar Outpost]],
+        blocking         = true,
+        category         = [[corpses]],
+        damage           = 250,
+        energy           = 0,
+        featureDead      = [[DEAD2]],
+        footprintX       = 3,
+        footprintZ       = 3,
+        metal            = 50,
+        object           = [[wrecks/bradartowerwreck.s3o]],
+        reclaimable      = true,
+        reclaimTime      = 1500,
+      },
 
 
+        DEAD2 = {
+        description      = [[Debris - Radar Outpost]],
+        blocking         = false,
+        category         = [[heaps]],
+        damage           = 125,
+        energy           = 0,
+        footprintX       = 3,
+        footprintZ       = 3,
+        metal            = 25,
+        object           = [[b3x3heap.s3o]],
+        reclaimable      = true,
+        reclaimTime      = 750,
+      },
 
-  featureDefs                   = {
 
-    DEAD  = {
-      description      = [[Wreckage - Radar Outpost]],
-      blocking         = true,
-      category         = [[corpses]],
-      damage           = 250,
-      energy           = 0,
-      featureDead      = [[DEAD2]],
-      footprintX       = 2,
-      footprintZ       = 2,
-      metal            = 50,
-      object           = [[wrecks/bradartowerwreck.s3o]],
-      reclaimable      = true,
-      reclaimTime      = 1500,
     },
-
-
-    DEAD2 = {
-      description      = [[Debris - Radar Outpost]],
-      blocking         = false,
-
-      category         = [[heaps]],
-      damage           = 125,
-      energy           = 0,
-      footprintX       = 2,
-      footprintZ       = 2,
-      metal            = 25,
-      object           = [[b2x2heap.s3o]],
-      reclaimable      = true,
-      reclaimTime      = 750,
-    },
-
-
-  },
 
 
 
